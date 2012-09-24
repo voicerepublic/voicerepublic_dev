b29d65f1e21fa6a4cb00e0fb0f36d9b3b786bdcf
rene paulokat <rene@so36.net>
2012-09-24 17:50:43 +0200

    gem and environment fix 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,8 @@ Kluuu2::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  #config.assets.compile = false
-  config.assets.compile = true
+  config.assets.compile = false
+  #config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -64,5 +64,5 @@ Kluuu2::Application.configure do
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
