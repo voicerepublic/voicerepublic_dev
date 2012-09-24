b0ddba629807babfb7fe8ddc40b7d04ee62bf710
rene paulokat <rene@so36.net>
2012-09-24 20:12:53 +0200

    latest klus first

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,7 +1,7 @@
 class LandingPageController < ApplicationController
   def index
-    @kluuus = Kluuu.published.paginate(:page => params[:page], :per_page => 6)
-    @no_kluuus = NoKluuu.published.paginate(:page => params[:page], :per_page => 6)
+    @kluuus = Kluuu.published.paginate(:page => params[:page], :per_page => 6).order("created_at DESC")
+    @no_kluuus = NoKluuu.published.paginate(:page => params[:page], :per_page => 6).order("created_at DESC")
   end
   
   
