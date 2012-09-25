07b68ee6da2a8008396b0b9ca1dccb5cb259660d
rene paulokat <rene@so36.net>
2012-09-25 17:55:33 +0200

    fix request-specs...

--- spec/requests/conversations_spec.rb
+++ spec/requests/conversations_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
+
 describe "Conversations" do
+  
+  before do
+    @user = FactoryGirl.create(:user) 
+  end
+  
   describe "GET /conversations" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get conversations_path
+      get user_conversations_path(:user_id => @user)
       response.status.should be(200)
     end
   end
--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
+
+
 describe "Messages" do
+  
+  before do
+    @user = FactoryGirl.create(:user) 
+  end
+  
   describe "GET /messages" do
+    
     it "works! (now write some real specs)" do
+      
+      
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get messages_path
-      response.status.should be(200)
+      get user_messages_path(:user_id => @user)
+      response.status.should be(302)
     end
   end
 end
