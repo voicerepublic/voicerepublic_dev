6fa3578aa610909b0ca72cf67d9219361866b5a0
rene paulokat <rene@so36.net>
2012-09-27 15:55:27 +0200

    semicolon too much...

--- app/models/notification/base.rb
+++ app/models/notification/base.rb
@@ -1,6 +1,6 @@
 class Notification::Base < ActiveRecord::Base
   
-  attr_accessible :content, :user_id, 
+  attr_accessible :content, :user_id
   attr_accessible :klu_id, :url, :other_id 
   
   belongs_to :user
