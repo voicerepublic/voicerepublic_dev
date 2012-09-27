84ea500d6e973a672a40a06ffeba50500ede0750
rene paulokat <rene@so36.net>
2012-09-27 17:33:41 +0200

    create notification if user has a new message

--- spec/models/notification/new_message_spec.rb
+++ spec/models/notification/new_message_spec.rb
@@ -9,6 +9,12 @@ describe Notification::NewMessage do
     FactoryGirl.build(:notification_new_message, other: nil).should_not be_valid
   end
   
-  it "will be created if there is a new message for user"
+  it "will be created if there is a new message for user" do
+    me = FactoryGirl.create(:user)
+    num_notifications = me.notifications.count 
+    other = FactoryGirl.create(:user)
+    f = FactoryGirl.create(:message, sender: other, receiver: me)
+    me.notifications.count.should eq(num_notifications + 1)
+  end
 
 end
