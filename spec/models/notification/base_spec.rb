c385f65af955d5d78ec01753b73cf304efc43584
rene paulokat <rene@so36.net>
2012-09-27 15:00:01 +0200

    simple spec

--- spec/models/notification/base_spec.rb
+++ spec/models/notification/base_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Notification::Base do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory do" do
+    FactoryGirl.create(:notification_basis).should be_valid
+  end
+
+  it "is invalid without user" do
+    FactoryGirl.build(:notification_basis, user: nil).should_not be_valid
+  end
+
 end
