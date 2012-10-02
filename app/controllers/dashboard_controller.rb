4208bdd551f7463241395e8fe8516fcc8cef01a6
rene paulokat <rene@so36.net>
2012-10-02 14:12:34 +0200

    rescue exception in case thinking-sphinx is not running...

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,12 @@
 class DashboardController < ApplicationController
-  
+
   before_filter :authenticate_user!
   before_filter :set_user
-  
   def index
     @user = current_user
     redirect_to :action => 'matches'
   end
-  
+
   def contacts
     @followed = @user.followed_relations
     @follower = @user.follower_relations
@@ -17,7 +16,7 @@ class DashboardController < ApplicationController
       format.json { render json: @follows }
     end
   end
-  
+
   def bookmarks
     @bookmarks = @user.bookmarks
     respond_to do |format|
@@ -25,7 +24,7 @@ class DashboardController < ApplicationController
       format.json { render json: @bookmarks }
     end
   end
-  
+
   def news
     @news = nil
     respond_to do |format|
@@ -33,28 +32,33 @@ class DashboardController < ApplicationController
       format.json { render json: @news }
     end
   end
-  
-  
-  def matches 
+
+  def matches
     @klus = @user.klus
-    if params[:id]
-      @matched_klu = Klu.find(params[:id])
-      @matching_klus = @matched_klu.complementaries 
-    else
-      unless @klus.empty?
-        @matched_klu = @klus.first
+    begin
+      if params[:id]
+        @matched_klu = Klu.find(params[:id])
         @matching_klus = @matched_klu.complementaries
+      else
+        unless @klus.empty?
+          @matched_klu = @klus.first
+          @matching_klus = @matched_klu.complementaries
+        end
       end
+    rescue Exception => e
+      logger.error("Dasboard#matches - problem with sphinx: #{e.inspect}")
+      @matching_klus = []
     end
+    flash.now.error "there is a problem with sphinx"
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @matches }
     end
   end
-  
+
   private
-  
+
   def set_user
     @user = current_user
   end
