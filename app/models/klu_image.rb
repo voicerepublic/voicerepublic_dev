9d1b7b4ab6f5741c151941520c1f1719999280d4
rene paulokat <rene@so36.net>
2012-10-09 17:10:53 +0200

    crop die zweite

--- app/models/klu_image.rb
+++ app/models/klu_image.rb
@@ -1,6 +1,6 @@
 class KluImage < ActiveRecord::Base
   attr_accessible :description, :klu_id, :image
-  has_attached_file :image, :styles => { :large => "760#x570#", :medium => "360#x270#", :thumb => "120#x90#" }
+  has_attached_file :image, :styles => { :large => "760x570#", :medium => "360x270#", :thumb => "120x90#" }
   
   belongs_to :kluuu, :class_name => 'Kluuu', :foreign_key => :klu_id
   
