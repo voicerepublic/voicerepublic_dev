85104c59b30a6d553b2f58a5c7308e81eb914967
imazined <timo@mazine.de>
2012-10-11 23:21:39 +0200

    User small klus as bookmarks

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -2,9 +2,9 @@ module BookmarksHelper
   
   def partial_for_bookmark(bookmark)
     if bookmark.kluuu.nil?  # if nil its a no_kluuu
-      render(:partial => "klus/medium_no_kluuu", :locals => {:klu => bookmark.no_kluuu })
+      render(:partial => "klus/small_no_kluuu", :locals => {:klu => bookmark.no_kluuu })
     else
-      render(:partial => "klus/medium_kluuu", :locals => {:klu => bookmark.kluuu })
+      render(:partial => "klus/small_kluuu", :locals => {:klu => bookmark.kluuu })
     end
   end
   
