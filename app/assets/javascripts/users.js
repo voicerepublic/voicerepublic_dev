8bd4998f558f61e8c59cd2e5a65344051511a921
imazined <timo@mazine.de>
2012-10-19 19:07:14 +0200

    Profil weitergemacht. Gutes Wóchenende

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,8 +1,23 @@
 (function($){
   $(function(){
+    $('.user-nav').append('<li><a href=\"#\" id=\"user-toggle\">Show more</a></li>');
+    var userAboutShownLines = 2;
+    var animationDuration = 300;
     var userInfo = $('.user-more-information').hide();
+    var userAbout = $('.user-about');
+    var userAboutOriginalHeight = userAbout.height();
+    var userAboutHiddenHeight = parseInt(userAbout.css("line-height"), 10) * userAboutShownLines;
+    var userAboutHiddenToggle  = true; 
+    userAbout.height(userAboutHiddenHeight + "px");
     $('#user-toggle').on('click', function() {
-      userInfo.slideToggle(300);
+      if (userAboutHiddenToggle) {
+        userAbout.animate({height: userAboutOriginalHeight}, animationDuration);
+        userAboutHiddenToggle = !userAboutHiddenToggle;
+      } else {
+        userAbout.animate({height: userAboutHiddenHeight + "px"}, animationDuration);
+        userAboutHiddenToggle = !userAboutHiddenToggle;
+      };
+      userInfo.slideToggle(animationDuration);
     });
   });
 })(jQuery);
