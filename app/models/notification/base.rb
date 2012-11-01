d0f12b94bbb6dda46fa548632c6c3b10a4251a6f
rene paulokat <rene@so36.net>
2012-11-01 20:27:39 +0100

    not ready - js-generation of notifies

--- app/models/notification/base.rb
+++ app/models/notification/base.rb
@@ -1,16 +1,14 @@
 class Notification::Base < ActiveRecord::Base
   
-  scope :unread, where(:read => false)
+  ALERTS          = %w{ Notification::NewBookmark 
+                        Notification::NewComment
+                        Notification::NewFollower
+                        Notification::NewRating } 
+  CONTENT_ALERTS  = %w{ Notification::NewKluuu Notification::NewStatus }
   
-  scope :alerts, :conditions => { :type => %w{ Notification::NewBookmark 
-                                               Notification::NewComment
-                                               Notification::NewFollower
-                                               Notification::NewRating
-                                              } 
-                                 }, :order => "created_at DESC"
-                                 
-  scope :content_alerts, 
-                :conditions => { :type => %w{ Notification::NewKluuu Notification::NewStatus }  }, :order => "created_at DESC"
+  scope :unread,          where(:read => false)
+  scope :alerts,          :conditions => { :type => ALERTS }, :order => "created_at DESC"
+  scope :content_alerts,  :conditions => { :type => CONTENT_ALERTS  }, :order => "created_at DESC"
   
   alias_method :reason, :to_s
   
@@ -24,7 +22,7 @@ class Notification::Base < ActiveRecord::Base
   def generate_push_notification
     Rails.logger.debug("#{self.class.name}#generate_push_notification - start")
     begin
-      set_notification_count
+      set_notification_count if self.instance_of?()
       #PrivatePub.publish_to("/notifications/#{user_id}", "alert('<%= self.to_s %>');")
     rescue Exception => e
       self.logger.error("#{self.class.name}#generate_push_notification - error: #{e.inspect}")
@@ -35,9 +33,25 @@ class Notification::Base < ActiveRecord::Base
   def generate_mail_notification
   end
   
+  # sets number of notifications unread in actionbar
+  #
   def set_notification_count
     js = "$('#alerts-count-#{user_id}').html('#{self.user.notifications.alerts.unread.count}');"
     Rails.logger.debug("#{self.class.name} - set_notification_count: js: '#{js}'")
+    PrivatePub.publish_to("/notifications/#{user_id}", js)
+  end
+  
+  # pushes notification into actionbar - notification-listing
+  #
+  def push_notification_in_actionbar
+    # $('<p>Test</p>').insertAfter('.inner');
+    
+    #<li class="menu-item">
+    #  <%= link_to_url_for_notification_reason(notification) do %>
+    #    <%= notification %>
+    #  <% end %>
+    #</li>
+    js = "$('<li class=\'menu-item\'>').insertAfter('#notifications-#{user_id}');"
     PrivatePub.publish_to("/notifications/#{user_id}", js)
   end
   
