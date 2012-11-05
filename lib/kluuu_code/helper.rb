9623fd6731214faf5003bb99f0afc03e3a575578
imazined <timo@mazine.de>
2012-11-05 18:28:06 +0100

    For Martin

--- lib/kluuu_code/helper.rb
+++ lib/kluuu_code/helper.rb
@@ -5,10 +5,11 @@ module KluuuCode
   module Helper
     class IpAddress
       def IpAddress.local_ip
-        if Socket.ip_address_list.detect{|intf| intf.ipv4? and !intf.ipv4_private? and !intf.ipv4_loopback? and !intf.ipv4_multicast?} && (Addrinfo.tcp(Socket.ip_address_list.detect{|intf| intf.ipv4? and !intf.ipv4_private? and !intf.ipv4_loopback? and !intf.ipv4_multicast?}.ip_address, 80).ip_address == Socket.getaddrinfo(Socket.gethostname,'http',nil,:STREAM)[0][2])
+        if Rails.env == 'production'
           return Socket.gethostname
-        elsif Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
-          return Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.ip_address
+        elsif Rails.env == 'test' || 'development'
+          tmp = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.ip_address + ':3000'
+          return tmp
         else
           raise 'No callback IP or hostname detected'
         end
