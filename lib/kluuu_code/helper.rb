e60d49fa60cfdd97b04f07148508db963bcac7a3
Martin Unger <basisklasse@orange.(none)>
2012-11-05 12:51:28 +0100

    added new ip address detection

--- lib/kluuu_code/helper.rb
+++ lib/kluuu_code/helper.rb
@@ -3,7 +3,16 @@ require 'socket'
 module KluuuCode
   
   module Helper
-    
+    class IpAddress
+      def IpAddress.local_ip
+        if Socket.ip_address_list.detect{|intf| intf.ipv4? and !intf.ipv4_private? and !intf.ipv4_loopback? and !intf.ipv4_multicast?} && (Addrinfo.tcp(Socket.ip_address_list.detect{|intf| intf.ipv4? and !intf.ipv4_private? and !intf.ipv4_loopback? and !intf.ipv4_multicast?}.ip_address, 80).ip_address == Socket.getaddrinfo(Socket.gethostname,'http',nil,:STREAM)[0][2])
+          return Socket.gethostname
+        elsif Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
+          return Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.ip_address
+        else
+          raise 'No callback IP or hostname detected'
+        end
+      end
+    end
   end
-  
 end
