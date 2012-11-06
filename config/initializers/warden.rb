b69e31ab64832ab3e95e3dd25052ba90d65f6dd1
rene paulokat <rene@so36.net>
2012-11-06 14:45:29 +0100

    care for nil value in warden

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,7 +1,7 @@
 Warden::Manager.after_authentication do |user,auth,opts|
-  user.update_attribute(:available, 'online')
+  user.update_attribute(:available, 'online') if user
 end
 
 Warden::Manager.before_logout do |user,auth,opts|
-  user.update_attribute(:available, 'offline')
+  user.update_attribute(:available, 'offline') if user
 end
