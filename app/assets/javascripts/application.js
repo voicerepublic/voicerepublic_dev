7b5a071e2d4b585b737e183a5fe11e9ebc311232
imazined <timo@mazine.de>
2012-11-09 18:48:33 +0100

    CheckUserOnline js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,41 @@
 //= require twitter/bootstrap
 //= require_tree .
 
+
+(function($){
+  var checkUserOnline = {
+    init: function() {
+      var url = "/users/online_user.json";
+
+      var setUserStatus = function(payload) {
+        console.log(payload);
+        $.each(payload, function(i) {
+          $('.user-image[data-user-id=' +  payload[i].id +']').addClass(payload[i].available);
+        });
+      };
+
+      var usersOnSite = $('.user-image'); //JqueryCollection
+      var userIDs = {};
+      $.each(usersOnSite, function() {
+        userIDs[$(this).data("user-id")] = "";
+      });
+      $.ajax({
+        url: url,
+        data: userIDs,
+        success:  function(data) {
+          setUserStatus(data);
+        },
+        error: function() { console.log("Connection Error"); }
+      });
+    }
+  };
+  $(function(){
+    checkUserOnline.init();
+  });
+})(jQuery);
+
+
+
 overlay = {
 
   build: function(innerHTML, force) {
@@ -133,7 +168,7 @@ function fitText(jquerySelector) {
       // console.log("Title " + text.html());
       // console.log("targetWidth: " + targetWidth);
       var inner = text.children('span');
-      console.log(inner + " " + inner.length);
+      // console.log(inner + " " + inner.length);
       if (inner.length === 0 ){
         inner = $("<span />").css({
           display: "block",
@@ -142,7 +177,7 @@ function fitText(jquerySelector) {
         text.wrapInner(inner);
         inner = text.children('span');
         inner = $(inner[0]);
-        console.log($(inner) + " " + inner.length);
+        // console.log($(inner) + " " + inner.length);
       };
       var currentFontSize = parseInt(text.css("font-size"), 10);
       // console.log(inner.text());
