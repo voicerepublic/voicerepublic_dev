1183bde5dd759ffd5009c0dc143a3d640ecc94cc
rene paulokat <rene@so36.net>
2012-11-09 14:48:33 +0100

    template for notification_missed_call and generate mail on missed call

--- spec/models/notification/missed_call_spec.rb
+++ spec/models/notification/missed_call_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Notification::MissedCall do
-  it "has a valid factory"
+  
+  it "has a valid factory" do
+    FactoryGirl.build(:notification_missed_call).should be_valid
+  end
+  
+  it "is invalid without supplied klu" do
+    FactoryGirl.build(:notification_missed_call, :klu => nil).should_not be_valid
+  end
+  
 end
