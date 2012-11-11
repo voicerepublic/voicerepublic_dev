a9c899b0a7b8adfc982feb89bf1d267b0f4e42d8
rene paulokat <rene@so36.net>
2012-11-11 23:22:15 +0100

    user-chat on the way...

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,15 +1,16 @@
-class ChatMessage
+class Chat
   extend ActiveModel::Callbacks
   extend ActiveModel::Naming
   include ActiveModel::Validations
+  include ActiveModel::Conversion
   include ActiveModel::Serializers::JSON
   
-  attr_accessor :user_id
-  attr_accessor :recipient_id
+  attr_accessor :user1  # is recipient
+  attr_accessor :user2  # is sender
   attr_accessor :body
   
-  validates :user_id, :presence => true
-  validates :recipient_id, :presence => true
+  validates :user2, :presence => true
+  validates :user1, :presence => true
   validates :body, :presence => true
   
   define_model_callbacks :save
@@ -23,13 +24,24 @@ class ChatMessage
     end
   end
   
-  
   def save
     if self.valid?
       run_callbacks :save do
         self
       end
     end
+  end
+  
+  def persisted?
+    false
+  end
+  
+  def recipient_for(user)
+    user == user1 ? user2 : user1
+  end
+  
+  def css_id_for_chat
+    "chat-" + [user1.id,user2.id].sort.join("-")
   end
   
   private
