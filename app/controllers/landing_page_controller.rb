16849287fd6de544ab65872fa82634d2c0ce5dfa
rene paulokat <rene@so36.net>
2012-11-22 19:59:17 +0100

    CLOSES #1299

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,16 +1,8 @@
 class LandingPageController < ApplicationController
+  
   def index
     @kluuus = Kluuu.published.paginate(:page => params[:page], :per_page => 6).order("created_at DESC")
     @no_kluuus = NoKluuu.published.paginate(:page => params[:page], :per_page => 6).order("created_at DESC")
-  end
-  
-  
-  def show_tagged_with
-    _klus = Klu.tagged_with(params[:tag])
-    @kluuus = []
-    @no_kluuus = []
-    _klus.each { |klu| klu.instance_of?(Kluuu) ? @kluuus << klu : @no_kluuus << klu }
-    render :action => 'index'
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Kluuu2::Application.routes.draw do
     post "bookmark/:klu_id", :controller => "bookmarks", :action => "create", :as => "create_bookmark"
     post "messages/:receiver_id", :controller => 'messages', :action => 'create', :as => 'create_message'
     get 'messages/:receiver_id/new', :controller => 'messages', :action => 'new', :as => 'new_message'
-    get "tags/:tag", :controller => 'landing_page', :action => 'show_tagged_with', :as => 'tagged_with'
+    get "tags/:tag", :controller => 'search', :action => 'tagged_with', :as => 'tagged_with'
     post "chats/:one/:two", :controller => 'chats', :action => 'create', :as => 'post_chat'
     get "chats/:one/:two/new", :controller => 'chats', :action => 'new', :as => 'new_chat'
     delete "chats/:one/:two", :controller => 'chats', :action => 'destroy', :as => 'destroy_chat'
