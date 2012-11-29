849ddfe76e31f18a0a99cf8e8391cb7b6ebd5abf
Martin Meyer <martin.meyer@gmx.ch>
2012-11-29 07:01:00 +0100

    prevent matches right height

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,6 +1,6 @@
 (function($){
   var $dashboardLeft = $('.dashboard-matches .dashboard-matches-own-klus');
-  var $dashboardRight = $('.dashboard-matches .dashboard-matches-own-klus + .tab-content');
+  var $dashboardRight = $('.matches-results');
   if ($dashboardLeft.length > 0) {
     if ($dashboardLeft.height() > $dashboardRight.height()) {
       $dashboardRight.height($dashboardLeft.height());
