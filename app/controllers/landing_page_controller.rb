17b72c3048ed68205f625721cbb0c35869c23c94
rene paulokat <rene@so36.net>
2012-12-04 22:34:45 +0100

    dont fetch no_kluuus when they wont be shown on index

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -2,7 +2,7 @@ class LandingPageController < ApplicationController
   
   def index
     @kluuus = Kluuu.published.paginate(:page => params[:page], :per_page => 8).order("created_at DESC")
-    @no_kluuus = NoKluuu.published.paginate(:page => params[:page], :per_page => 8).order("created_at DESC")
+    #@no_kluuus = NoKluuu.published.paginate(:page => params[:page], :per_page => 8).order("created_at DESC")
   end
   
 end
