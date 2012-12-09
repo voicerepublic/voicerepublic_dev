6e21fbaa49a0131b6c232b5835ae08196b91d5d8
rene paulokat <rene@so36.net>
2012-12-09 21:43:39 +0100

    add whenever to hook again

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ default_run_options[:pty] = true
 #before 'deploy:update_code', 'sphinx:stop'
 after "deploy:restart", "deploy:cleanup"
 after "deploy:setup", "dbconf:setup" 
-after "deploy:finalize_update", "dbconf", 'sphinx:symlink_indexes' #, 'sphinx:start'
+after "deploy:finalize_update", "dbconf", 'sphinx:symlink_indexes', 'whenever:update_crontab' #, 'sphinx:start'
 #after 'deploy:update_code'#, 'sphinx:start'
 
 
