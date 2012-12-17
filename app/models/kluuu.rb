e66ba6f78796904aa46fec0b177b9a8dbd9be6bb
rene paulokat <rene@so36.net>
2012-12-17 20:52:28 +0100

    CLOSES #1425

--- app/models/kluuu.rb
+++ app/models/kluuu.rb
@@ -23,24 +23,24 @@ class Kluuu < Klu
     unless self.user.balance_account.nil?
       self.currency = self.user.balance_account.currency
     else
-      self.errors.add(:currency, I18n.t('.no_account', :default => 'You got to create an balance account before You can charge for your KluuUs'))
+      self.errors.add(:currency, I18n.t('model_kluuu.no_account', :default => 'You got to create an balance account before You can charge for your KluuUs'))
     end
   end
   
   def check_charge
     if charge_type == 'minute'
       unless charge_cents < 501 || charge_cents > 9
-        self.errors.add(:charge_cents, I18n.t('.charge_per_minute_error', :default => "Minimum 10 and maximum 500"))
+        self.errors.add(:charge_cents, I18n.t('model_kluuu.charge_per_minute_error', :default => "Minimum 10 and maximum 500"))
       end
     end
     if charge_type == 'fix'
       unless charge_cents < 5001 || charge_cents > 99
-        self.errors.add(:charge_cents, I18n.t('.charge_fixed_error', :default => "Minimum 100 and maximum 5000"))
+        self.errors.add(:charge_cents, I18n.t('model_kluuu.charge_fixed_error', :default => "Minimum 100 and maximum 5000"))
       end
     end
     if charge_type == 'free'
       if charge_cents > 0 || charge_cents < 0
-        self.errors.add(:charge_cents, I18n.t('.charge_free_error', :default => "You should leave the amount at zero if this is a free kluuu"))
+        self.errors.add(:charge_cents, I18n.t('model_kluuu.charge_free_error', :default => "You should leave the amount at zero if this is a free kluuu"))
       end
     end
   end
