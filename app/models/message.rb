4b29c5e7d4435e8a386504011e50915e5331a819
rene paulokat <rene@so36.net>
2013-01-10 16:14:49 +0100

    fix wrong url

--- app/models/message.rb
+++ app/models/message.rb
@@ -44,7 +44,7 @@ class Message < ActiveRecord::Base
   end
   
   def generate_notification
-    Notification::NewMessage.create(:other_id => sender.id, :user_id => receiver.id, :url => Rails.application.routes.url_helpers.user_conversation_url(:user_id => sender, :id => self.conversation, :only_path => true))
+    Notification::NewMessage.create(:other_id => sender.id, :user_id => receiver.id, :url => Rails.application.routes.url_helpers.user_conversation_url(:user_id => receiver, :id => self.conversation))
   end
   
 end
