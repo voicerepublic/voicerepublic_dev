954a05a81365556cd34bc839aa84271cdcb3ba6e
rene paulokat <rene@so36.net>
2013-02-01 15:03:03 +0100

    dashboard venues and landing-page

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -11,9 +11,18 @@ class Venue < ActiveRecord::Base
   
   after_create :generate_notification
   
+  MIN_TIME = 240
   DURATION = [ 30, 60, 90, 120, -1 ]
   REPEATING = { I18n.t('model_venue.no_repeat') => 0, I18n.t('model_venue.weekly') => 1, I18n.t('model_venue.biweekly') => 2, I18n.t('model_venue.monthly') => 3 }
   
+  def timed_out?
+    min = ( duration < 0 ) ? 240 : duration
+    ( start_time.in_time_zone + min.minutes ) <= Time.now.in_time_zone 
+  end
+  
+  def ongoing?
+    ! timed_out?  && ( start_time.in_time_zone + runtime.minutes ) >= Time.now.in_time_zone &&  start_time.in_time_zone < Time.now.in_time_zone
+  end
   
   def self.of_user(user)
     Venue.joins(:host_kluuu => :user).where("user_id = ?", user.id)
@@ -41,6 +50,10 @@ class Venue < ActiveRecord::Base
   end
   
   private
+  
+  def runtime
+    ( duration < 0 ) ? MIN_TIME : duration
+  end
   
   def generate_notification
     
