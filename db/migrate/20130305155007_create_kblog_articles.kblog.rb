a2ad7616761444025b5d3f7642e7d0b618eecb56
rene paulokat <rene@so36.net>
2013-03-07 20:30:17 +0100

    blog engine included

---
+++ db/migrate/20130305155007_create_kblog_articles.kblog.rb
@@ -0,0 +1,11 @@
+# This migration comes from kblog (originally 20130303192309)
+class CreateKblogArticles < ActiveRecord::Migration
+  def change
+    create_table :kblog_articles do |t|
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
