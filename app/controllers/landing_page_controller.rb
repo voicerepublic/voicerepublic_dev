50bdad8a682fafe9e092f1fa4d954d6a20ba0c11
rene paulokat <rene@so36.net>
2013-03-25 19:59:46 +0100

    check if news is existent - otherwise don't display news-headline - add pagination

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,7 +1,7 @@
 class LandingPageController < ApplicationController
   
   def index
-    @kluuus = Kluuu.published.paginate(:page => params[:page], :per_page => 4).order("created_at DESC")
+    @kluuus = Kluuu.published.paginate(:page => params[:page], :per_page => 8).order("created_at DESC")
     #@no_kluuus = NoKluuu.published.paginate(:page => params[:page], :per_page => 8).order("created_at DESC")
   end
   
--- app/helpers/landing_page_helper.rb
+++ app/helpers/landing_page_helper.rb
@@ -1,4 +1,20 @@
 module LandingPageHelper
   include ActsAsTaggableOn::TagsHelper
   
+  
+  # checks if there is an upcoming venue
+  # additionally checks for available Kblog::Articles
+  #
+  def partial_for_venue_and_news
+    
+    if Venue.upcoming
+      if Kblog::Article.first.nil?
+        render(:partial => 'lp_venue')
+      else 
+        render(:partial => 'lp_venue_news')
+      end
+    end
+    
+  end
+  
 end
