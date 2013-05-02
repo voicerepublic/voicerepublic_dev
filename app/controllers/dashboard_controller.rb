17fb28309757c9fe1c7b587924505db69d3ac681
Rene Paulokat <rene@so36.net>
2013-05-02 23:10:21 +0200

    rip dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,7 @@ class DashboardController < ApplicationController
   
   def index
     @user = current_user
-    redirect_to :action => 'matches'
+    redirect_to :action => 'news'
   end
 
   def contacts
