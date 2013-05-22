661ed00676cc911c348761526390cb4492ceca94
Martin Meyer <martin.meyer@gmx.ch>
2013-05-22 11:52:32 +0200

    klu bookmark enhancement

--- app/helpers/klus_helper.rb
+++ app/helpers/klus_helper.rb
@@ -5,12 +5,12 @@ module KlusHelper
     #return nil if user.new_record?
     if user && klu && (klu.user != user) &! klu.user.nil?
       if Bookmark.bookmarked?(user.id, klu.id)
-        link_to(user_bookmark_path(:user_id => user, :id => Bookmark.bookmark_for(user.id, klu.id).id), :method => :delete, :title => t('.delete_bookmark', :default => "Delete Bookmark"), :class => "klu-bookmark-delete") do
-          content_tag('i', '', :class => 'icon-bookmark')
+        link_to(user_bookmark_path(:user_id => user, :id => Bookmark.bookmark_for(user.id, klu.id).id), :method => :delete, :title => t('.delete_bookmark_title'), :class => "klu-bookmark-delete") do
+          content_tag('i', '', :class => 'icon-bookmark') + content_tag('i', '', :class => 'icon-bookmark-empty')
         end
       else
-        link_to(create_bookmark_path(:klu_id => klu), :method => :post, :title => t('.add_bookmark', :default => "Add Bookmark"), :class => "klu-bookmark-add") do
-          content_tag('i', '', :class => 'icon-bookmark-empty')
+        link_to(create_bookmark_path(:klu_id => klu), :method => :post, :title => t('.add_bookmark_title'), :class => "klu-bookmark-add") do
+          content_tag('i', '', :class => 'icon-bookmark-empty') + content_tag('i', '', :class => 'icon-bookmark') + content_tag(:span, t('.add_bookmark'), :class => 'bookmark-text')
         end
       end
     end
