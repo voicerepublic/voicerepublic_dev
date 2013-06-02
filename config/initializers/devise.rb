4b6a3155d6ea792ac56e00ded8d289a4822bf55b
Rene Paulokat <rene@so36.net>
2013-06-02 14:18:19 +0200

    comment out remember period

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -105,10 +105,11 @@ Devise.setup do |config|
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
+  #Devise::TRUE_VALUES << ["on"]
   config.remember_for = 2.weeks
 
   # If true, extends the user's remember period when remembered via cookie.
-  config.extend_remember_period = true
+  #config.extend_remember_period = true
 
   # Options to be passed to the created cookie. For instance, you can set
   # :secure => true in order to force SSL only cookies.
