ffcc50b875b4894571b31f401ba7cf476932552b
Martin Meyer <martin.meyer@gmx.ch>
2013-06-05 22:47:07 +0200

    on user direct to kluuu view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    @kluuus = @user.kluuus
 
     respond_to do |format|
       format.html # show.html.erb
