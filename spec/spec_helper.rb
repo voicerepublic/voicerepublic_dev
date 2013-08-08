78af92af97cd7ec09d9519e4183c995e32e09f3b
Martin Meyer <martin.meyer@gmx.ch>
2013-08-08 11:34:05 +0200

    new articles and comment layout

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,9 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 
 RSpec.configure do |config|
+
+  config.color_enabled = true
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
