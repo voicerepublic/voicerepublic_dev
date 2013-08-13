4191064a0da45682c65078544d44705e69ae7825
phil <phil@branch14.org>
2013-08-13 10:47:58 +0200

    ad record field to event model and forms (#11535)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130808140805) do
+ActiveRecord::Schema.define(:version => 20130813083653) do
 
   create_table "accounts", :force => true do |t|
     t.string   "timezone"
@@ -129,6 +129,7 @@ ActiveRecord::Schema.define(:version => 20130808140805) do
     t.integer  "venue_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "record"
   end
 
   add_index "events", ["start_time"], :name => "index_events_on_start_time"
