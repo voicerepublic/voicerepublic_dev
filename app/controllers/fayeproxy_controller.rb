df95f6505844b6c48508b2a8eefa496ab03216e6
phil <phil@branch14.org>
2013-08-19 11:25:15 +0200

    fix fayproxy, really render nothing

--- app/controllers/fayeproxy_controller.rb
+++ app/controllers/fayeproxy_controller.rb
@@ -2,7 +2,7 @@ class FayeproxyController < ApplicationController
 
   def publish
     PrivatePub.publish_to(params[:channel], params[:data])
-    render status: :ok
+    render nothing: true, status: :ok
   end
 
 end
