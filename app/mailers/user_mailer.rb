336cb24c5844c191b5c82b9814acfa3d00a9c261
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2013-08-27 15:06:26 +0200

    change email from info@kluuu.com to service@kluuu.com

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,18 +1,18 @@
 class UserMailer < ActionMailer::Base
-  default from: "info@kluuu.com"
-  
+  default from: "service@kluuu.com"
+
   def friend_notification(notification)
     @notification = notification
     @user = @notification.user
     I18n.locale = @user.account.preferred_locale
     mail(:to => @user.email, :subject => t('mailers.your_friend_took_action'))
   end
-  
+
   def content_notification(notification)
     @notification = notification
     @user = @notification.user
     I18n.locale = @user.account.preferred_locale
     mail(:to => @user.email, :subject => t('mailers.activity_around_you'))
   end
-  
+
 end
