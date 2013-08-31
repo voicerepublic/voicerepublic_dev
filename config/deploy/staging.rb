e4413d46ce3ceb8014de1a59d724d55091145c32
Andrew Djoga <andrew.djoga@gmail.com>
2013-08-31 17:39:59 +0300

    Broken staging message

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,5 @@
+raise "TEMPORARY BROKEN: need to set up correct paths to private pubs etc, \nFOR PRODUCTION USE: cap production deploy:migrations"
+
 server 'kluuu-production.panter.ch', :app, :web, :db, :primary => true
 set :branch, "develop"
 set :rails_env, "production"
