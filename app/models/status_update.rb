cb18528d5d02893805def696ec63e670494e2eac
Andrew Djoga <andrew.djoga@gmail.com>
2013-09-19 13:01:48 +0300

    Disable status history because of comments conflict and needs in refactoring!

--- app/models/status_update.rb
+++ app/models/status_update.rb
@@ -2,7 +2,6 @@ class StatusUpdate < ActiveRecord::Base
   attr_accessible :content, :user_id
 
   belongs_to :user
-  has_many :comments, :as => :commentable, :dependent => :destroy, :order => "created_at DESC"
 
   validates :user_id, :presence => true
   validates :content, :presence => true
