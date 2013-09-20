ad27a2f4bc475ac3998a5a9b61f66d22f559f857
Andrew Djoga <andrew.djoga@gmail.com>
2013-09-20 19:42:02 +0300

    Change facebook redirect

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -17,7 +17,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
       @user.update_attribute(:available, 'online')
       remember_me(@user)
-      sign_in_and_redirect @user, :event => :authentication
+      sign_in @user, :event => :authentication
+      redirect_to venues_url
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
