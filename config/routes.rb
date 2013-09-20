fd1c41b6bac0d3202172f251fe26aa4d1cbe8e98
Andrew Djoga <andrew.djoga@gmail.com>
2013-09-20 16:30:27 +0300

    Impl sign up form on the landing page

--- config/routes.rb
+++ config/routes.rb
@@ -63,8 +63,8 @@ Kluuu2::Application.routes.draw do
       delete "/users/sign_out" => "devise/sessions#destroy"
     end
     devise_for :users, :controllers => {  :omniauth_callbacks => "users/omniauth_callbacks",
-                                          :sessions => "users/sessions"
-                                          #:registrations => "users/registrations" 
+                                          :sessions => "users/sessions",
+                                          :registrations => "users/registrations"
                                         }
   end
   
