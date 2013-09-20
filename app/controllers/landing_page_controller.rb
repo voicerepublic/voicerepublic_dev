8c05cfd99e9b29d4225f5b3826ceaee0628eec22
Andrew Djoga <andrew.djoga@gmail.com>
2013-09-20 16:07:45 +0300

    Implement login form on the landing page

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -4,6 +4,7 @@ class LandingPageController < ApplicationController
     #@kluuus = Kluuu.published.paginate(:page => params[:page], :per_page => 8).order("created_at DESC")
     #@no_kluuus = NoKluuu.published.paginate(:page => params[:page], :per_page => 8).order("created_at DESC")
     @venues = Venue.not_past
+    @user = User.new
   end
   
 end
