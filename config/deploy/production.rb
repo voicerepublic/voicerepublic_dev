7bbfb4e7083220c8b378e283c11403085336e7dc
Andrew Djoga <andrew.djoga@gmail.com>
2013-09-25 18:08:34 +0300

    Deployment TODOs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,5 @@
+#TODO nginx startup: ~/nginx-rtmp/nginx-1.5.3/objs/nginx -p ~/nginx-rtmp/nginx-run/ -c ~/nginx-rtmp/nginx-run/conf/nginx.conf
+#TODO RAILS_ENV=production bundle exec rackup -d private_pub.ru -s thin -E production -P tmp/pids/faye.pid -D
+
 server 'kluuu-production.panter.ch', :app, :web, :db, :primary => true
 set :branch, "develop"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,6 @@
+#TODO nginx startup: ~/nginx-rtmp/nginx -p /home/rails/nginx-rtmp -c ~/nginx-rtmp/nginx.conf
+#TODO RAILS_ENV=production bundle exec rackup -d private_pub.ru -s thin -E production -P tmp/pids/faye.pid -D
+
 server 'kluuu-staging.panter.ch', :app, :web, :db, :primary => true
 set :branch, "develop"
 set :rails_env, "production"
