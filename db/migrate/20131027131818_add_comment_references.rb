4bb6b9cf7041505b61a049c18f316a3ae938f70f
Munen Alain M. Lafon <munen@dispatched.ch>
2013-10-27 14:21:33 +0100

    add commentable to comments table
    this should have been here already, but was not in the last production database
    dump that has been used to restore the server. so this is a manual fix.

---
+++ db/migrate/20131027131818_add_comment_references.rb
@@ -0,0 +1,17 @@
+class AddCommentReferences < ActiveRecord::Migration
+  def up
+    change_table :comments do |t|
+      t.references :commentable, :polymorphic => true
+    end
+
+    add_index :comments, :commentable_type
+    add_index :comments, :commentable_id
+  end
+
+  def down
+    change_table :comments do |t|
+      t.remove :commentable_type
+      t.remove :commentable_id
+    end
+  end
+end
