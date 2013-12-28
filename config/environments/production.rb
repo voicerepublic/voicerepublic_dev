b81ef228f943dace11697b7d56e60d9957428025
phil <phil@branch14.org>
2013-12-28 12:49:24 +0100

    change recipient for exception notifications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,8 @@ Kluuu2::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.middleware.use ExceptionNotifier,
-                            :email_prefix         => "[Kluuu2] ",
-                            :sender_address       => %{"notifier" <rails@kluuu.com>},
-                            :exception_recipients => %w{pho@panter.ch},
+                            :email_prefix         => "[VR] ",
+                            :sender_address       => %{"notifier" <notifier@voicerepublic.com>},
+                            :exception_recipients => %w{tech@voicerepublic.com},
                             :ignore_crawlers      => %w{Googlebot}
 end
