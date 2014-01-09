64e77ceb894412db6053af5b5a9d11202658f8fc
phil <phil@branch14.org>
2014-01-09 15:41:55 +0100

    change ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131027131818) do
+ActiveRecord::Schema.define(:version => 20131129191735) do
 
   create_table "accounts", :force => true do |t|
     t.string   "timezone"
@@ -356,11 +356,9 @@ ActiveRecord::Schema.define(:version => 20131027131818) do
   add_index "venue_klus", ["venue_id"], :name => "index_venue_klus_on_venue_id"
 
   create_table "venues", :force => true do |t|
-    t.integer  "host_kluuu_id"
     t.datetime "start_time"
     t.text     "description"
     t.string   "title"
-    t.string   "intro_video"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
     t.integer  "duration"
