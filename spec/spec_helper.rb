52c28570eb4760502e7efc9d011bdd679734319a
phil <phil@branch14.org>
2014-01-20 14:42:22 +0100

    add check for pending migrations for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,11 +10,13 @@ require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
 
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
 
