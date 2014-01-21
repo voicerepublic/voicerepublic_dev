49d0e9c300e96995516b90fbe685d6bdce6eaa30
phil <phil@branch14.org>
2014-01-21 16:03:12 +0100

    deploy master branch on live

--- config/deploy/live.rb
+++ config/deploy/live.rb
@@ -15,7 +15,7 @@
 # something that quacks like a hash can be used to set
 # extended properties on the server.
 server 'kluuu.com', user: 'rails', roles: %w{web app db}
-set :branch, 'cap3'
+set :branch, 'master'
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
