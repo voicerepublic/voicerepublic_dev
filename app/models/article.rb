cc0a75b2c55ea621f36e651c6445ae49fed5b3a0
phil <phil@branch14.org>
2014-01-22 11:24:19 +0100

    add 4 specs for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,14 +2,14 @@ class Article < ActiveRecord::Base
 
   acts_as_paranoid
 
+  attr_accessible :content
+
+  validates :venue, :user, :content, :presence => true
+
   has_many :comments, dependent: :destroy
   belongs_to :venue
   belongs_to :user
 
   delegate :user, to: :venue, prefix: true
-
-  attr_accessible :content
-
-  validates :venue, :user, :content, :presence => true
 
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,15 +2,49 @@ require 'spec_helper'
 
 describe Article do
 
-  it "should hav a valid factory" do
-    FactoryGirl.build(:article).should be_valid
+  describe 'built' do
+
+    before do 
+      @article = FactoryGirl.build(:article)
+    end
+
+    it "has a valid factory" do
+      expect(@article).to be_valid
+    end
+    
+    it 'validates presence of venue' do
+      @article.venue = nil
+      expect(@article).to_not be_valid
+    end
+    
+    it 'validates presence of user' do
+      @article.user = nil
+      expect(@article).to_not be_valid
+    end
+    
+    it 'validates presence of content' do
+      @article.content = nil
+      expect(@article).to_not be_valid
+    end
+    
   end
 
-  it 'should set deleted_at on deletions' do
-    article = FactoryGirl.create :article
-    expect { article.delete }.to change { Article.count }.by(-1)
-    expect(Article.only_deleted).to include(article)
-    expect(Article.all).to_not include(article)
-  end
+  describe 'created' do
 
+    before do
+      @article = FactoryGirl.create(:article)
+    end
+
+    it 'delegates to venue_user' do
+      expect(@article.venue_user).to eq(@article.venue.user)
+    end
+    
+    it 'should set deleted_at on deletions' do
+      expect { @article.delete }.to change { Article.count }.by(-1)
+      expect(Article.only_deleted).to include(@article)
+      expect(Article.all).to_not include(@article)
+    end
+    
+  end
+  
 end
