36f977b44e630741cc01ebf4cd716c4b17a22673
phil <phil@branch14.org>
2014-02-11 14:41:42 +0100

    authentication and authorization for api/talks_controller

--- app/controllers/api/talks_controller.rb
+++ app/controllers/api/talks_controller.rb
@@ -1,5 +1,6 @@
 class Api::TalksController < ApplicationController
 
+  before_action :authenticate_user!
   before_action :set_talk
 
   # delegate to a sepcific message handler or complain
@@ -14,21 +15,21 @@ class Api::TalksController < ApplicationController
     either = state || event
     return render text: 'Neither `state` nor `event` given.', status: 422 unless either
 
+    gfy = state && current_or_guest_user.id != @user_id.to_i
+    gfy = gfy || event && current_or_guest_user != @talk.user
+    return render text: 'Computer says no', status: 740 if gfy
+
     @method = either.underscore
     @method = :store_state if state && !respond_to?(@method)
 
     # TODO check for security issue (whitelist methods)
-    # TODO check if current_or_guest_user is the host of the talk if event
-    if respond_to? @method
-      msg = send(@method, msg)
-    end
+    msg = send @method, msg if respond_to? @method
 
     publish msg.to_hash
     head :ok
   end
 
   def store_state(msg)
-    # TODO check if current_user.id is @user_id (use before_action)
     Talk.transaction do
       session = @talk.reload.session || {}
       session[@user_id][:state] = msg[:state]
@@ -39,14 +40,14 @@ class Api::TalksController < ApplicationController
 
   def registering(msg)
     details = nil
-    # TODO check if current_user.id is @user_id (use before_action)
     user = User.find(@user_id)
     Talk.transaction do
       session = @talk.reload.session || {}
       session[@user_id] = details = user.details_for(@talk).merge state: 'Registering'
       @talk.update_attribute :session, session
     end
-    msg[:user].merge details # merge additional info
+    msg[:user].merge! details # merge additional info
+    msg
   end
 
   def set_talk
