12b0b95e284c5fdfb52898b6324bcd6b5ce0b33d
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2014-02-14 20:30:18 +0100

    selenium, phantomjs, firefox with firebug, vcr fix for phantom and a first spec

---
+++ spec/features/talks_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "Talks" do
+
+  describe "validation" do
+
+    before do
+      @venue = FactoryGirl.create :venue
+      @talk = FactoryGirl.create :talk, venue: @venue, tag_list: "test, foo, bar"
+    end
+
+    it "does not lose tags on failed validation", js: true, driver: :chrome do
+      visit edit_venue_talk_path 'en', @venue, @talk
+      fill_in :talk_title, with: ""
+      click_on I18n.t 'helpers.submit.submit'
+      page.should have_content "Please review the problems below"
+      within '#s2id_talk_tag_list.tagList' do
+        page.should have_content "test"
+        page.should have_content "foo"
+        page.should have_content "bar"
+      end
+    end
+
+  end
+
+end
