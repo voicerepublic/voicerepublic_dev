3fcf00d6fe104138e1e4dcaa74fb5e4e06791a49
phil <phil@branch14.org>
2014-03-03 10:00:28 +0100

    two new strategies: move_clean & jinglize

---
+++ lib/audio/strategy/jinglize.rb
@@ -0,0 +1,45 @@
+# Merges the resulting wav with the start and stop jingle.
+#
+# Prior to this strategy use MoveClean.
+#
+module Audio
+  module Strategy
+    class Jinglize < Base
+
+      def input
+        "#{name}.wav"
+      end
+
+      # checked as a precondition
+      def inputs
+        [ input ] + jingles
+      end
+
+      def backup
+        "#{name}-bak.wav"
+      end
+
+      def run
+        FileUtils.mv(input, backup)
+        merge_with_jingles
+        outputs
+      end
+
+      def outputs
+        [ input, backup ]
+      end
+
+      def merge_with_jingles_cmd
+        start, stop = jingles
+        "sox -m #{start} #{backup} #{stop} #{input}"
+      end
+
+      def jingles
+        # FIXME: expecting rails env
+        [ Rails.root.join('app/assets/sounds/start_jingle.wav'),
+          Rails.root.join('app/assets/sounds/stop_jingle.wav') ]
+      end
+
+    end
+  end
+end
