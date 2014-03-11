52b3d6755cb92a80cbf0f6f52098f6ecd9bcc31d
phil <phil@branch14.org>
2014-03-11 11:45:34 +0100

    fix doc

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,7 +7,7 @@ class SearchController < ApplicationController
     redirect_to "/search/1/" + u(params[:query])
   end
   
-  # GET  /search/:query
+  # GET  /search/1/:query
   def show
     @results = PgSearch.multisearch(params[:query]).
       paginate(page: params[:page], per_page: PER_PAGE)
