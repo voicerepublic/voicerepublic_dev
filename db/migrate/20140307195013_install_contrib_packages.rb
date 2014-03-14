c25f61b2d0edd79aee73809d3f3b9debfca0c9af
phil <phil@branch14.org>
2014-03-14 14:02:24 +0100

    omit creating pg extensions

--- db/migrate/20140307195013_install_contrib_packages.rb
+++ db/migrate/20140307195013_install_contrib_packages.rb
@@ -1,13 +1,13 @@
 class InstallContribPackages < ActiveRecord::Migration
   def up
-    execute "CREATE EXTENSION pg_trgm;"
-    execute "CREATE EXTENSION unaccent;"
-    # execute "CREATE EXTENSION fuzzystrmatch;"
+    #execute "CREATE EXTENSION pg_trgm;"
+    #execute "CREATE EXTENSION unaccent;"
+    ## execute "CREATE EXTENSION fuzzystrmatch;"
   end
 
   def down
-    execute "DROP EXTENSION pg_trgm;"
-    execute "DROP EXTENSION unaccent;"
-    # execute "DROP EXTENSION fuzzystrmatch;"
+    #execute "DROP EXTENSION pg_trgm;"
+    #execute "DROP EXTENSION unaccent;"
+    ## execute "DROP EXTENSION fuzzystrmatch;"
   end
 end
