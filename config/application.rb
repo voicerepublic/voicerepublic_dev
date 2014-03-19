c590a034a05f0500b0003764e70c139502ba7a63
phil <phil@branch14.org>
2014-03-19 15:25:06 +0100

    Revert "temporarily disable assets precomp. config, to see how that works out"
    This reverts commit 7f9031bc6f21414ba7d864b8301561a4e5dfc0cc.

--- config/application.rb
+++ config/application.rb
@@ -62,12 +62,11 @@ module VoiceRepublic
     # still needed with rails4 ?
     config.assets.paths << "#{config.root}/app/assets/fonts"
 
-    # TODO deploy without and remove if everything is fine
-    # config.assets.precompile += %w( *.js *.png *.jpg )
-    # # TODO use assets the right way and we won't need these hacks, ask phil
-    # config.assets.precompile += %w(
-    #   livepage.js
-    # )
+    config.assets.precompile += %w( *.js *.png *.jpg )
+    # TODO use assets the right way and we won't need these hacks, ask phil
+    config.assets.precompile += %w(
+      livepage.js
+    )
   end
 end
 
