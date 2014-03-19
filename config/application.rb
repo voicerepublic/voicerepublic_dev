7f9031bc6f21414ba7d864b8301561a4e5dfc0cc
phil <phil@branch14.org>
2014-03-19 15:20:12 +0100

    temporarily disable assets precomp. config, to see how that works out

--- config/application.rb
+++ config/application.rb
@@ -62,11 +62,12 @@ module VoiceRepublic
     # still needed with rails4 ?
     config.assets.paths << "#{config.root}/app/assets/fonts"
 
-    config.assets.precompile += %w( *.js *.png *.jpg )
-    # TODO use assets the right way and we won't need these hacks, ask phil
-    config.assets.precompile += %w(
-      livepage.js
-    )
+    # TODO deploy without and remove if everything is fine
+    # config.assets.precompile += %w( *.js *.png *.jpg )
+    # # TODO use assets the right way and we won't need these hacks, ask phil
+    # config.assets.precompile += %w(
+    #   livepage.js
+    # )
   end
 end
 
