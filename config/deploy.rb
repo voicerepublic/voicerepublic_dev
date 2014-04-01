1043e087152572ba3fdab0e511935b128d61a22b
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2014-04-01 20:21:44 +0200

    update linked files for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{ config/database.yml }
+set :linked_files, %w{ config/database.yml config/private_pub.yml }
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache
