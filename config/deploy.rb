4a19e4d425d504bb8b2742f3cefdaa815eafbe32
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2014-04-02 16:55:33 +0200

    better deployment of settings to different stages

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{ config/database.yml config/private_pub.yml }
+set :linked_files, %w{ config/database.yml config/private_pub.yml config/settings.local.yml }
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache
