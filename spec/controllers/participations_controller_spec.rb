891d74de24efef20b9ad5b26bd9c332be35c4632
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2014-04-10 00:35:42 +0200

    participate for guest users requires sign_up

--- spec/controllers/participations_controller_spec.rb
+++ spec/controllers/participations_controller_spec.rb
@@ -1,21 +1,38 @@
 require 'spec_helper'
 
 describe ParticipationsController do
-  
+
   describe 'with user logged in' do
-    
+
     # login user
     before  do
       @user = FactoryGirl.create(:user)
       request.env['warden'].stub :authenticate! => @user
       controller.stub :current_user => @user
     end
-    
+
     def valid_attributes
       {}
     end
-    
+
     describe 'POST create' do
+      before { @venue = FactoryGirl.create(:venue) }
+      describe 'guests' do
+        it 'leads guests to the signup form' do
+          @user.update_attribute :guest, true
+          request.env['warden'].stub :authenticate! => @user
+          controller.stub :current_user => @user
+          Participation.count.should eq(0)
+          post :create, { venue_id: @venue.id, participation: valid_attributes }
+          response.should redirect_to(new_user_registration_path)
+          Participation.count.should eq(0)
+        end
+
+        it 'leads actual users back to where they came from' do
+          post :create, { venue_id: @venue.id, participation: valid_attributes }
+          response.should redirect_to(@venue)
+        end
+      end
       describe 'with valid parameters' do
         it 'creates a new Participation' do
           @venue = FactoryGirl.create(:venue)
@@ -38,7 +55,7 @@ describe ParticipationsController do
         end
       end
     end
-    
+
   end
 
 end
