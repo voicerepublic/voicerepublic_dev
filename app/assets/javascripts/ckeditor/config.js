1ec5685fd0c5b548cbbdd7bf3f9a25710eb52738
Martin Meyer <martin.meyer@gmx.ch>
2014-04-16 16:49:17 +0200

    make richtext editor more rich

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -5,23 +5,24 @@ CKEDITOR.editorConfig = function( config ) {
 
   // The toolbar groups arrangement, optimized for two toolbar rows.
   config.toolbarGroups = [
-  { name: 'basicstyles', groups: [ 'basicstyles' ] },
-  { name: 'styles' , groups: [ 'Format' ] },
-  { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-  { name: 'links', groups : [ 'Link','Unlink' ] },
-
-
+    { name: 'basicstyles', groups: [ 'basicstyles' ] },
+    { name: 'styles' , groups: [ 'Format' ] },
+    { name: 'links', groups : [ 'Link','Unlink' ] },
+    { name: 'paragraph', groups : [ 'list', 'indent' ] },
+    { name: 'document',    groups: [ 'mode' ] },
   ];
 
   config.format_tags = 'p;h3';
 
   // Remove some buttons, provided by the standard plugins, which we don't
   // need to have in the Standard(s) toolbar.
-  config.removeButtons = 'Strike,Underline,Subscript,Superscript,Cut,Copy,Paste,Undo,Redo,Font,Blockquote,FontSize,Styles,Anchor';
+  config.removeButtons = 'Save,NewPage,Preview,Print,Strike,Underline,Subscript,Superscript,Cut,Copy,Paste,Undo,Redo,Font,Blockquote,FontSize,Styles,Anchor';
 
   // Language
   config.language = 'en';
   config.height = '300px';
+
+  config.forcePasteAsPlainText = true;
 
   // Remove html statusbar
   removePlugins: 'elementspath'
@@ -36,3 +37,22 @@ CKEDITOR.editorConfig = function( config ) {
     });
   }
 })();
+
+  // config.toolbar_Full = [
+  //   { name: 'document',    groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source', 'Save', 'NewPage', 'DocProps', 'Preview', 'Print', 'Templates', 'document' ] },
+  //   // On the basic preset, clipboard and undo is handled by keyboard.
+  //   // Uncomment the following line to enable them on the toolbar as well.
+  //   // { name: 'clipboard',   groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', 'Undo', 'Redo' ] },
+  //   { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', 'SelectAll', 'Scayt' ] },
+  //   { name: 'insert', items: [ 'CreatePlaceholder', 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe', 'InsertPre' ] },
+  //   { name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
+  //   '/',
+  //   { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', 'RemoveFormat' ] },
+  //   { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ], items: [ 'NumberedList', 'BulletedList', 'Outdent', 'Indent', 'Blockquote', 'CreateDiv', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', 'BidiLtr', 'BidiRtl' ] },
+  //   { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+  //   '/',
+  //   { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+  //   { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+  //   { name: 'tools', items: [ 'UIColor', 'Maximize', 'ShowBlocks' ] },
+  //   { name: 'about', items: [ 'About' ] }
+  // ];
