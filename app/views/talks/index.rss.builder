5d70dfbeb8fda3cd5120bbbfd44e7a3ece8f3028
Rene Paulokat <rene@so36.net>
2014-04-21 14:00:03 +0200

    WIP

--- app/views/talks/index.rss.builder
+++ app/views/talks/index.rss.builder
@@ -1,11 +1,16 @@
+@audio_format ||= 'ogg'
 xml.instruct! 
 xml.rss version: '2.0', 'xmlns:atom' => 'http://www.w3.org/2005/Atom' do
   xml.channel do
     xml.title @venue.title
     xml.description h @venue.description
     xml.link venue_url(@venue)
-    xml.tag! 'atom:link', rel: 'self', type: 'application/rss+xml', href: venue_url(@venue)
-    #xml.artwork
+    xml.image do
+      xml.url "#{request.protocol}#{request.host_with_port}#{@venue.image.url}"
+      xml.title @venue.title
+      xml.link venue_url(@venue)
+    end
+    #xml.tag! 'atom:link', rel: 'self', type: 'application/rss+xml', href: venue_talks_url(@venue, format: :rss)
     @venue.talks.each do |talk|
       if talk.archived?
         xml.item do
@@ -15,7 +20,9 @@ xml.rss version: '2.0', 'xmlns:atom' => 'http://www.w3.org/2005/Atom' do
           xml.link venue_talk_url(@venue, talk)
           xml.guid venue_talk_url(@venue, talk)
           talk.media_links.each_pair do |k,v|
-            xml.enclosure url: "http://voicerepublic.com#{v}", type: k, length: talk.duration
+            xml.enclosure url: "http://voicerepublic.com#{v}", 
+                          type: "audio/#{k}", 
+                          length: talk.duration if k == @audio_format
           end
         end
       end
--- spec/requests/podcast_feed_spec.rb
+++ spec/requests/podcast_feed_spec.rb
@@ -14,19 +14,41 @@ describe 'podcast feed' do
   end
 
   describe 'audio format' do
-    it 'format mp3' do
+    it 'default ogg' do
       get venue_talks_path(@venue, format: :rss)
+      expect(response.body).to include("voicerepublic.com/vrmedia/#{@talks.first.id}.ogg")
+    end
+
+    it 'format mp3' do
+      get venue_talks_path(@venue, format: :rss, audio_format: 'mp3')
       expect(response.body).to include("voicerepublic.com/vrmedia/#{@talks.first.id}.mp3")
     end
     
     it 'format m4a' do
-      get venue_talks_path(@venue, format: :rss)
+      get venue_talks_path(@venue, format: :rss, audio_format: 'm4a')
       expect(response.body).to include("voicerepublic.com/vrmedia/#{@talks.first.id}.m4a")
     end
     
     it 'format ogg' do
-      get venue_talks_path(@venue, format: :rss)
+      get venue_talks_path(@venue, format: :rss, audio_format: 'ogg')
       expect(response.body).to include("voicerepublic.com/vrmedia/#{@talks.first.id}.ogg")
     end
+
   end
+
+  describe 'contains' do
+    it 'only :archived' do
+      talk = FactoryGirl.create(:talk, venue: @venue)
+      get venue_talks_path(@venue, format: :rss, audio_format: 'ogg')
+      expect(response.body).to_not include("#{talk.id}.ogg")
+    end
+
+    it 'venue image' do
+      get venue_talks_path(@venue, format: :rss)
+      expect(response.body).to include(@venue.image.url)
+    end
+    
+  end
+
+
 end
