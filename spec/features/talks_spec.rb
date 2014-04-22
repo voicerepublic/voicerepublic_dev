0f07f89865d501d9cd16408c33811eb88d8287ae
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2014-04-22 14:53:32 +0200

    fix specs and localise

--- spec/features/talks_spec.rb
+++ spec/features/talks_spec.rb
@@ -7,6 +7,52 @@ describe "Talks" do
     login_user(@user)
   end
 
+  describe "Exploring Talks" do
+
+    before do
+      @talk = FactoryGirl.create(:talk)
+    end
+
+    describe "as user on all pages" do
+      it 'shows explore in talk_path' do
+        visit talk_path(@talk)
+        page.should have_selector('.explore')
+        page.should have_content('Explore')
+      end
+
+      it 'shows explore in venue_talk_path' do
+        visit venue_talk_path(@talk.venue, @talk)
+        page.should have_selector('.explore')
+        page.should have_content('Explore')
+      end
+
+      it 'shows explore in user_path' do
+        visit user_path(@user)
+        page.should have_selector('.explore')
+        page.should have_content('Explore')
+      end
+    end
+
+    describe "visiting talks#index" do
+      it 'displays talks' do
+        visit talks_path
+        page.should have_selector('.explore-talks')
+        page.should have_content I18n.t('shared.explore.explore_talks')
+      end
+
+      it 'displays 25 talks a time' do
+        FactoryGirl.create_list(:talk, 26)
+        visit talks_path
+        page.should have_selector('.talk-medium-text-box', count: 25)
+        page.should have_selector('.pagination')
+        within(".pagination") do
+          page.should have_link('2')
+          page.should_not have_link('3')
+        end
+      end
+    end
+  end
+
   describe "Talk#new" do
     it 'creates a new talk', driver: :chrome do
       venue = FactoryGirl.create(:venue, user: @user)
