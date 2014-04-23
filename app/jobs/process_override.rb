a6c8c2bdd3bdc415141d93de0bd85e82f7f9cc93
phil <phil@branch14.org>
2014-04-23 16:28:01 +0200

    Yo Dawg, do not create delayed jobs from inside delayed jobs

--- app/jobs/process_override.rb
+++ app/jobs/process_override.rb
@@ -1,5 +1,5 @@
 class ProcessOverride < Struct.new(:talk_id)
   def perform
-    Talk.find(talk_id).delay(queue: 'audio').process_override!
+    Talk.find(talk_id).send(:process_override!)
   end
 end
--- app/jobs/reprocess.rb
+++ app/jobs/reprocess.rb
@@ -1,5 +1,5 @@
 class Reprocess < Struct.new(:talk_id)
   def perform
-    Talk.find(talk_id).delay(queue: 'audio').reprocess!
+    Talk.find(talk_id).send(:reprocess!)
   end
 end
