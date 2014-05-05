2a465481be7704366b473a230495841ed6dafde6
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2014-05-05 02:00:18 +0200

    errbit: enable js notifier

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -3,4 +3,5 @@ Airbrake.configure do |config|
   config.host    = 'voicerepublic-errbit.herokuapp.com'
   config.port    = 80
   config.secure  = config.port == 443
+  config.js_notifier = true
 end
