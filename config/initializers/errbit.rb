0b33e59e36d4926c6a69795390cd98dfa4f3ae92
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2014-05-05 02:13:31 +0200

    errbit: remove js error tracking

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -3,5 +3,4 @@ Airbrake.configure do |config|
   config.host    = 'voicerepublic-errbit.herokuapp.com'
   config.port    = 80
   config.secure  = config.port == 443
-  config.js_notifier = true
 end
