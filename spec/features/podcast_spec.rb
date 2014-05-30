37c56425689568cada4aa29861413e13398cb914
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2014-05-30 21:44:38 +0200

    specs, cleanup, more testing. still needs styling

--- spec/features/podcast_spec.rb
+++ spec/features/podcast_spec.rb
@@ -40,18 +40,51 @@ feature 'Podcast' do
   feature 'for venue on venues#show' do
 
     let(:venue) { FactoryGirl.create(:venue) }
-    
+
     scenario 'venues#show' do
       visit venue_path(venue)
       links = Nokogiri::HTML(source).xpath(rss)
       expect(links).to_not be_empty
     end
-    
+
     scenario 'venues#show.rss' do
       visit venue_path(venue, format: 'rss')
       page.should have_xpath('//rss')
     end
-    
+
+  end
+
+  feature "different OSs see different protocol URLs" do
+
+    let(:venue) { FactoryGirl.create(:venue) }
+
+    describe "ITPC protocol" do
+      scenario "safari 7" do
+        user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9) AppleWebKit/537.71 (KHTML, like Gecko) Version/7.0 Safari/537.71"
+        page.driver.browser.header('User-Agent', user_agent)
+        visit venue_path(venue)
+        page.should have_xpath("//a[contains(@href,'itpc')]")
+      end
+      scenario "iPad" do
+        user_agent = "Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25"
+        page.driver.browser.header('User-Agent', user_agent)
+        visit venue_path(venue)
+        page.should have_xpath("//a[contains(@href,'feed')]")
+      end
+      scenario "iPad" do
+        user_agent = "Mozilla/5.0 (iPhone; U; ru; CPU iPhone OS 4_2_1 like Mac OS X; ru) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148a Safari/6533.18.5"
+        page.driver.browser.header('User-Agent', user_agent)
+        visit venue_path(venue)
+        page.should have_xpath("//a[contains(@href,'feed')]")
+      end
+      scenario "Firefox/Linux" do
+        user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:28.0) Gecko/20100101 Firefox/28.0"
+        page.driver.browser.header('User-Agent', user_agent)
+        visit venue_path(venue)
+        page.should have_xpath("//a[contains(@href,'http://www.example.com/venues/1.rss')]")
+      end
+    end
+
   end
 
 end
