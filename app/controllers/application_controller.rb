e0ef9cd3521d3c8cddd811549ad8d6fa87578184
phil <phil@branch14.org>
2014-06-11 16:03:05 +0200

    add 3 0s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -102,7 +102,7 @@ class ApplicationController < ActionController::Base
   end
 
   def create_guest_user
-    token = SecureRandom.random_number(10000)
+    token = SecureRandom.random_number(10000000)
     name = ['guest', Time.now.to_i, token ] * '_'
     logger.debug "\033[31mCREATE GUEST USER: #{name}\033[0m"
     user = User.create( email: "#{name}@example.com",
