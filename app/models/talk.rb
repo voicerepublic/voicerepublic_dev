193d59c9d5a2f5789afa07564fc454be56774078
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2014-06-16 20:08:11 +0200

    add guard - defensive programming

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -279,6 +279,7 @@ class Talk < ActiveRecord::Base
 
   # Upload file to S3
   def upload_file(key, file)
+    return unless key and file
     handle = File.open(file)
     file = media_storage.files.create key: key, body: handle, content_type: content_type_for(key)
   end
