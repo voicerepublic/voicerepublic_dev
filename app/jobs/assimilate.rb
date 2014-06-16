3ca6280b2bc227fd6433243f81619fffc84678d2
phil <phil@branch14.org>
2014-06-16 11:20:36 +0200

    drafts version of running specs via assimilate

--- app/jobs/assimilate.rb
+++ app/jobs/assimilate.rb
@@ -1,6 +1,105 @@
+# Assimilate is part of Endless Assimilation, which is like Continuous
+# Integration, but more borg style.
+#
+# opts example:
+#
+#    ref: refs/heads/experimental/71854768/endless-assimilation
+#    after: 6f093fb8103f2459592e0c0f15bce3834d85aa2e
+#    before: '0000000000000000000000000000000000000000'
+#    created: true
+#    deleted: false
+#    forced: true
+#    compare: https://github.com/munen/voicerepublic_dev/commit/6f093fb8103f
+#    commits:
+#    - id: 6f093fb8103f2459592e0c0f15bce3834d85aa2e
+#      distinct: true
+#      message: initial job for endless assimilation
+#      timestamp: '2014-06-16T10:48:35+02:00'
+#      url: https://github.com/munen/voicerepublic_dev/commit/6f093fb8103f2459592e0c0f15bce3834d85aa2e
+#      author:
+#        name: phil
+#        email: phil@branch14.org
+#        username: branch14
+#      committer:
+#        name: phil
+#        email: phil@branch14.org
+#        username: branch14
+#      added:
+#      - app/jobs/assimilate.rb
+#      removed: []
+#      modified: []
+#    head_commit:
+#      id: 6f093fb8103f2459592e0c0f15bce3834d85aa2e
+#      distinct: true
+#      message: initial job for endless assimilation
+#      timestamp: '2014-06-16T10:48:35+02:00'
+#      url: https://github.com/munen/voicerepublic_dev/commit/6f093fb8103f2459592e0c0f15bce3834d85aa2e
+#      author:
+#        name: phil
+#        email: phil@branch14.org
+#        username: branch14
+#      committer:
+#        name: phil
+#        email: phil@branch14.org
+#        username: branch14
+#      added:
+#      - app/jobs/assimilate.rb
+#      removed: []
+#      modified: []
+#    repository:
+#      id: 13351188
+#      name: voicerepublic_dev
+#      url: https://github.com/munen/voicerepublic_dev
+#      description: VoiceRepublic
+#      homepage: http://voicerepublic.com
+#      watchers: 0
+#      stargazers: 0
+#      forks: 0
+#      fork: false
+#      size: 47308
+#      owner:
+#        name: munen
+#        email: munen@voicerepublic.com
+#      private: true
+#      open_issues: 15
+#      has_issues: true
+#      has_downloads: true
+#      has_wiki: true
+#      language: Ruby
+#      created_at: 1381001815
+#      pushed_at: 1402908548
+#      master_branch: integration
+#    pusher:
+#      name: branch14
+#      email: phil@branch14.org
+#    
 class Assimilate < Struct.new(:opts)
   def perform
+ 
+    url = opts['repository']['url']
+    ref = opts['ref']
+
     # do the ci magic
+    path = File.join(ENV['HOME'], 'ci')
+    FileUtils.mkdir_p(path) unless File.exist?(path)
+    Dir.chdir(path)
+
+    if File.directory?('repo')
+      Dir.chdir('repo')
+      %x[ git pull ]
+    else
+      %x[ git clone #{url} repo ]
+      Dir.chdir('repo')
+    end
+
+    # TODO steal some tricks from mina deployment
+    # https://github.com/mina-deploy/mina/tree/master/lib/mina
+
+    output = ''
+    output += %x[ git checkout #{ref} ]
+    output += %x[ bundle ]
+    output += %x[ rspec spec ]
+    
   end
 end
 
