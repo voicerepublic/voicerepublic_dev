2898b7c4fb84728f4fed3617c482ee86a67c09e9
phil <phil@branch14.org>
2014-06-17 14:43:48 +0200

    register required mime types

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,17 +1,5 @@
-# MIME::Types would return the wrong mime type for mp4. Making sure all our
-# content-types are reported correctly.
-class MIME::Types
-  def type_for(filename, platform = false)
-    file_suffix = filename.split(".").last.downcase
-    super unless ['m4a', 'mp3', 'ogg', 'flv'].include? file_suffix
+Mime::Type.register "audio/mp4",   :m4a
+Mime::Type.register "audio/mpeg",  :mp3
+Mime::Type.register "audio/ogg",   :ogg
+Mime::Type.register "video/x-flv", :flv
 
-    mime_type = case file_suffix
-    when "m4a" then "audio/mp4"
-    when "mp3" then "audio/mpeg"
-    when "ogg" then "audio/ogg"
-    when "flv" then "video/x-flv"
-    end
-
-    [ MIME::Type.new(mime_type)]
-  end
-end
