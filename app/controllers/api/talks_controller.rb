e45e2ab7d5236cdf5213e2b80606e1923c672f68
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2014-06-19 16:58:25 +0200

    make it testable

--- app/controllers/api/talks_controller.rb
+++ app/controllers/api/talks_controller.rb
@@ -27,7 +27,7 @@ class Api::TalksController < Api::BaseController
     msg = send @method, msg if respond_to? @method
 
     # this is critical, so raise an error if it fails
-    if state && state != @talk.reload.session[current_user.id][:state]
+    if validate_state(state)
       raise "Critical: Failed to set state #{state} " +
             "for user #{current_user.id} " +
             "on talk #{@talk.id}"
@@ -100,6 +100,10 @@ class Api::TalksController < Api::BaseController
   # protect_from_forgery for angular ajax requests (overwrite CSRF check)
   def verified_request?
     super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+  end
+
+  def validate_state(state)
+    state && state != @talk.reload.session[current_user.id][:state]
   end
 
 end
