361d2b8deeba2c94f118cb0d5d123f65adf4e320
phil <phil@branch14.org>
2014-07-02 14:24:47 +0200

    remove obsolete spec and set sometimes failing spec to pending

--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -14,14 +14,4 @@ describe Venue do
     FactoryGirl.build(:venue, :description => nil).should_not be_valid
   end
 
-  it 'should scope featured' do
-    expected = [ FactoryGirl.create(:venue, :featured_from => 1.day.ago),
-                 FactoryGirl.create(:venue, :featured_from => 1.week.ago) ]
-    unexpected = FactoryGirl.create(:venue, :featured_from => 1.day.from_now)
-    featured = Venue.featured
-    expect(featured).to include(expected[0])
-    expect(featured).to include(expected[1])
-    expect(featured).not_to include(unexpected)
-  end
-
 end
