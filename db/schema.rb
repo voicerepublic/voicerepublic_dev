0fa20e2794c628b622a286915e3d171484c25e59
phil <phil@branch14.org>
2014-07-02 15:51:58 +0200

    add rake task to generate missing slugs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140702131058) do
+ActiveRecord::Schema.define(version: 20140702134835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
