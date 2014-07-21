6b4bb84fc6f37ceee53e259fee6f6bdbeff62cc5
phil <phil@branch14.org>
2014-07-21 16:09:24 +0200

    simple implementation of blocking by user agent

---
+++ app/middlewares/blocker.rb
@@ -0,0 +1,18 @@
+# TODO read disallowed useragent/path combinations from robots.txt
+# and check all, rename this class to ForceRobots and publish
+#
+# There 704 (Goto Fail) error code was taken from
+#
+#     https://github.com/joho/7XX-rfc
+#
+class Blocker < Struct.new(:app, :opts)
+
+  def call(env)
+    return app.call(env) unless env['HTTP_USER_AGENT'].match(/EasouSpider/)
+    
+    [ 704, {"Content-Type" => "text/html"},
+      [ "<h1>704 Goto Fail</h1>",
+        "<p>Next time respect /robots.txt</p>" ] ]
+  end
+
+end
