9fc5b0ee06905dc3d9560309401ddad3f0464273
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2014-07-29 14:22:53 +0200

    rewrite mocking data - initial try

--- app/assets/javascripts/spec/support/config_mock.js
+++ app/assets/javascripts/spec/support/config_mock.js
@@ -1,81 +1,135 @@
 Livepage.factory("config", function() {
   return {
-    "talk_id": 42,
-    "host": "kluuu master",
-    "title": "Great Talk on Ecoprimitivism",
-    "teaser": "Ted was right!",
-    "session": {
-      "1": {
-        "id": 1,
-        "name": "kluuu master",
-        "role": "participant",
+   "user": {
+        "id": 42,
+        "name": "guest guest_6ecc09f2-07d9-4c5c-9a9b-428e260e694a",
+        "role": "listener",
         "image": "http://lorempixel.com/80/80/people/0/",
-        "stream": "t42-u1",
-        "state": "Hosting"
+        "stream": "t1-u42",
+        "channel": "/t1/u42"
       },
-      "341471": {
-        "id": 341471,
-        "name": "blablub blabla",
-        "role": "participant",
-        "image": "http://lorempixel.com/80/80/people/5/",
-        "stream": "t42-u341471",
-        "state": "WaitingForPromotion"
+      "initial_state": "Registering",
+      "statemachine": [
+        {
+          "name": "Registered",
+          "from": "Registering",
+          "to": "Waiting"
+        },
+        {
+          "name": "TalkStarted",
+          "from": "Waiting",
+          "to": "Listening"
+        },
+        {
+          "name": "MicRequested",
+          "from": "Listening",
+          "to": "ExpectingPromotion"
+        },
+        {
+          "name": "Promoted",
+          "from": "ExpectingPromotion",
+          "to": "OnAir"
+        },
+        {
+          "name": "Promoted",
+          "from": "Listening",
+          "to": "AcceptingPromotion"
+        },
+        {
+          "name": "PromotionAccepted",
+          "from": "AcceptingPromotion",
+          "to": "OnAir"
+        },
+        {
+          "name": "PromotionDeclined",
+          "from": "AcceptingPromotion",
+          "to": "Listening"
+        },
+        {
+          "name": "Demoted",
+          "from": "OnAir",
+          "to": "Listening"
+        },
+        {
+          "name": "Registered",
+          "from": "GuestRegistering",
+          "to": "OnAir"
+        },
+        {
+          "name": "Registered",
+          "from": "HostRegistering",
+          "to": "HostOnAir"
+        },
+        {
+          "name": "TalkEnded",
+          "from": "*",
+          "to": "Loitering"
+        }
+      ],
+      "talk_id": 1,
+      "host": "Munen Lafon",
+      "title": "asfd",
+      "teaser": "asdf",
+      "session": {
+        "8": {
+          "id": 8,
+          "name": "Munen Lafon",
+          "role": "host",
+          "image": "http://localhost:3000/media/W1siZmYiLCIvaG9tZS9tdW5lbi9zcmMvdm9pY2VyZXB1YmxpYy9hcHAvYXNzZXRzL2ltYWdlcy9kZWZhdWx0cy91c2VyLWF2YXRhci5qcGciXSxbInAiLCJ0aHVtYiIsIjEwMHgxMDAjbnciXV0/user-avatar.jpg?sha=40350d82",
+          "stream": "t1-u8",
+          "channel": "/t1/u8",
+          "state": "HostOnAir"
+        }
       },
-      "341473": {
-        "id": 341473,
-        "name": "munen 無念",
-        "role": "participant",
-        "image": "http://lorempixel.com/80/80/people/8/",
-        "stream": "t42-u341473",
-        "state": "ListeningButReady"
-      }
-    },
-    "fayeClientUrl": "http://172.21.21.171:9292/faye/client.js",
-    "fayeUrl": "http://172.21.21.171:9292/faye",
-    "subscription": {
-      "server": "http://172.21.21.171:9292/faye",
-      "timestamp": 1392201113767,
-      "channel": "/t42/public",
-      "signature": "9202ab90baecca5fac9ad6e208fed49928401701"
-    },
-    "namespace": "t42",
-    "fullname": "blablub blabla",
-    "user_id": 341471,
-    "handle": "u341471",
-    "role": "listener",
-    "statemachine": [
-      {
-        "name": "Registered",
-        "from": "Registering",
-        "to": "Listening"
+      "talk": {
+        "state": "live",
+        "remaining_seconds": -522160,
+        "starts_in": -524000,
+        "ends_in": -522160,
+        "links": {
+          "mp3": "/vrmedia/1.mp3",
+          "m4a": "/vrmedia/1.m4a",
+          "ogg": "/vrmedia/1.ogg"
+        },
+        "duration": 1800,
+        "channel": "/t1/public"
       },
-      {
-        "name": "RequestedMic",
-        "from": "Listening",
-        "to": "WaitingForPromotion"
+      "starts_at": 1406110620,
+      "ends_at": 1406112420,
+      "fayeClientUrl": "http://localhost:9292/faye/client.js",
+      "fayeUrl": "http://localhost:9292/faye",
+      "subscriptions": {
+        "/t1/public": {
+          "server": "http://localhost:9292/faye",
+          "timestamp": 1406634620248,
+          "channel": "/t1/public",
+          "signature": "9ee821121123304427e836bde919612c22dda9cf"
+        },
+        "/t1/u42": {
+          "server": "http://localhost:9292/faye",
+          "timestamp": 1406634620249,
+          "channel": "/t1/u42",
+          "signature": "7df35ffa0097e43720003fa93bff2a8e6781deff"
+        }
       },
-      {
-        "name": "Promoted",
-        "from": "WaitingForPromotion",
-        "to": "OnAir"
-      },
-      {
-        "name": "Promoted",
-        "from": "Listening",
-        "to": "OnAir"
-      },
-      {
-        "name": "Demoted",
-        "from": "OnAir",
-        "to": "ListeningButReady"
-      },
-      {
-        "name": "Promoted",
-        "from": "ListeningButReady",
-        "to": "OnAir"
-      }
-    ],
-    "stream": "t42-u341471",
-    "streaming_server": "rtmp://kluuu.com/recordings"
-  };
+      "namespace": "t1",
+      "fullname": "guest guest_6ecc09f2-07d9-4c5c-9a9b-428e260e694a",
+      "user_id": 42,
+      "handle": "u42",
+      "role": "listener",
+      "stream": "t1-u42",
+      "streaming_server": "rtmp://localhost/record",
+      "discussion": [
+
+      ],
+      "guests": [
+
+      ],
+      "participants": [
+
+      ],
+      "blackbox": {"silence_level":0,"silence_timeout":2000},
+      "loopback": null,
+      "blackbox_path": "/assets/Blackbox11.swf"
+    };
 });
