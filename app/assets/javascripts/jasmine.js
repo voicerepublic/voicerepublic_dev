ee36409f19de7e122292954323b6ffbede2de339
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2014-07-29 13:45:54 +0200

    more asset pipeline integration

--- app/assets/javascripts/jasmine.js
+++ app/assets/javascripts/jasmine.js
@@ -1,4 +1,8 @@
 //= require application
+//= require livepage
+//= require spec/support/angular-mocks
+//= require spec/support/config_mock
+//= require spec/livepage_spec
 (function($){
   // will be called on load
 })(jQuery);
