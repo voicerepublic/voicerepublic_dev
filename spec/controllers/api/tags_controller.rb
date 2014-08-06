56d9a0e39a0a1aba5a69798f598d0f776f8addcc
phil <phil@branch14.org>
2014-08-06 14:40:40 +0200

    cleanup spec

--- spec/controllers/api/tags_controller.rb
+++ spec/controllers/api/tags_controller.rb
@@ -9,7 +9,6 @@ describe Api::TagsController do
       get :tags, q: 'maunz'
       response.status.should be(200)
       res = JSON.parse(response.body)
-      JSON.parse(response.body)
 
       res['tags'].first['name'].should == 'maunzbraunz'
     end
