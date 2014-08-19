56eef8e6b34845c66bf5379fc5818bfaa14d96b0
Martin Meyer <martin@voicerepublic.com>
2014-08-19 14:53:10 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -64,8 +64,8 @@ ActiveRecord::Schema.define(version: 20140812100435) do
   create_table "comments", force: true do |t|
     t.text     "content"
     t.integer  "user_id",          null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.integer  "commentable_id"
     t.string   "commentable_type"
   end
@@ -104,8 +104,8 @@ ActiveRecord::Schema.define(version: 20140812100435) do
   create_table "participations", force: true do |t|
     t.integer  "venue_id"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "participations", ["user_id"], name: "index_participations_on_user_id", using: :btree
@@ -209,8 +209,8 @@ ActiveRecord::Schema.define(version: 20140812100435) do
   create_table "users", force: true do |t|
     t.string   "firstname"
     t.string   "lastname"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -243,8 +243,8 @@ ActiveRecord::Schema.define(version: 20140812100435) do
   create_table "venues", force: true do |t|
     t.text     "description"
     t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.string   "teaser"
     t.integer  "user_id"
     t.text     "options",     default: "--- {}\n"
