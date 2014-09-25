efcce6d1bd247258537fa92dc5a80c1e87001413
phil <phil@branch14.org>
2014-09-25 15:34:37 +0200

    fix spec by replacind vcr casettes

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,6 +1,6 @@
 class Slack < Struct.new(:chan, :user, :icon)
 
-  def send(message, opts)
+  def send(message, opts={})
 
     _chan = opts[:chan] || chan
     _user = opts[:user] || user
@@ -9,14 +9,14 @@ class Slack < Struct.new(:chan, :user, :icon)
     url = "https://voicerepublic.slack.com/services/hooks/incoming-webhook"+
           "?token=#{Settings.slack.token}"
     # message = "#{Settings.slack.tag} #{message}" if Settings.slack.tag
-      
+
     payload = {
       channel:    _chan,
       username:   _user,
       text:       message,
       icon_emoji: _icon
     }
-    
+
     json = JSON.unparse(payload)
     cmd = "curl -X POST --data-urlencode 'payload=#{json}' '#{url}' 2>&1"
     %x[ #{cmd} ]
