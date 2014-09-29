3900c72389fee0d2f295b6e313625cb42915bd48
Martin Meyer <martin@voicerepublic.com>
2014-09-29 12:11:19 +0200

    fix merge conflicts 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910143945) do
+ActiveRecord::Schema.define(version: 20140923092213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -234,6 +234,7 @@ ActiveRecord::Schema.define(version: 20140910143945) do
     t.string   "website"
     t.boolean  "conference"
     t.string   "authentication_token"
+    t.integer  "default_venue_id"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", using: :btree
