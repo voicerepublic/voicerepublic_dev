7b06e5656119e8a296a24346c60f5ca482020988
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2015-02-09 18:10:37 +0100

    try to improve on player start time

--- app/helpers/embed_player_helper.rb
+++ app/helpers/embed_player_helper.rb
@@ -17,7 +17,8 @@ module EmbedPlayerHelper
     background  = if talk.archived?
                     "&top1=#{CGI.escape(background_picture(talk))}" + "|10|10" +
                     "&autoplay=1" +
-                    "&autoload=1"
+                    "&buffer=10" +
+                    "&showplayer=always"
                   else
                     '&title=' + I18n.t('flash_player.not_archived_yet')
                   end
