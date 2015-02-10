f539fa96d88be47f370069e9a292bc01a6c2a38a
phil <phil@branch14.org>
2015-02-10 16:03:03 +0100

    prefer categories over non-category tags

--- app/controllers/xhr/tags_controller.rb
+++ app/controllers/xhr/tags_controller.rb
@@ -1,7 +1,7 @@
 class Xhr::TagsController < Xhr::BaseController
 
   def index
-    scope = ActsAsTaggableOn::Tag.where(["name ILIKE ?", "%#{params[:q]}%"])
+    scope = ActsAsTaggableOn::Tag.where(["name ILIKE ?", "%#{params[:q]}%"]).order(:category)
     tags = scope.paginate(:page => params[:page], :per_page => params[:limit] || 10)
     render json: { tags: tags, total: scope.count }
   end
