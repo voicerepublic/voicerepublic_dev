ccb5f887e36a282841856dda13ea347d1eede738
phil <phil@branch14.org>
2015-02-11 09:58:08 +0100

    fixed some specs

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,5 +1,7 @@
 class UploadsController < ApplicationController
 
+  before_filter :authenticate_user!
+
   def new
     attrs = params[:talk] ? talk_params : {}
     attrs[:venue_id] ||= current_user.default_venue_id
@@ -8,4 +10,5 @@ class UploadsController < ApplicationController
 
   def create
   end
+
 end
--- spec/controllers/uploads_controller_spec.rb
+++ spec/controllers/uploads_controller_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 
 describe UploadsController do
 
+  before do
+    @user = FactoryGirl.create :user
+    # log in user
+    request.env['warden'].stub :authenticate! => @user
+    controller.stub :current_user => @user
+  end
+
   describe "GET 'new'" do
     it "returns http success" do
       get 'new'
