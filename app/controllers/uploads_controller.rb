d880d0b8b5581bf806ffb9e311c523dae99f6898
phil <phil@branch14.org>
2015-02-13 19:52:27 +0100

    improving separation but still draft

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,6 +1,7 @@
-class UploadsController < ApplicationController
+class UploadsController < BaseController
 
-  before_filter :authenticate_user!
+  before_action :authenticate_user!
+  before_action :set_presigned_post_url
 
   def new
     attrs = params[:talk] ? talk_params : {}
@@ -8,7 +9,44 @@ class UploadsController < ApplicationController
     @talk = Talk.new(attrs)
   end
 
+  # POST /uploads
   def create
+    @talk = Talk.new(talk_params)
+    @talk.venue_user = current_user
+
+    authorize! :create, @talk
+
+    if @talk.save
+      redirect_to @talk, notice: 'Talk was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  # Only allow a trusted parameter "white list" through.
+  def talk_params
+    params.require(:talk).permit(:title, :teaser, :starts_at_date,
+                                 :starts_at_time,
+                                 :description, :image,
+                                 :tag_list, :language,
+                                 :new_venue_title, :venue_id,
+                                 :user_override_uuid)
+  end
+
+  # this is a phony fake presigned url generator!
+  # TODO make a less phony fake...
+  # In S3, permissions are set for everyone to upload. It's better to create
+  # a real presigned URL. Either use 'fog', or the 'aws-sdk' gem.
+  def set_presigned_post_url
+    @presigned_s3_post_url = "https://#{Settings.talk_upload_bucket}.s3.amazonaws.com"
+
+    # upload_bucket = AWS::S3.new.buckets[Settings.talk_upload_bucket]
+    #
+    # @presigned_s3_post_url = upload_bucket.presigned_post(key: "#{SecureRandom.uuid}",
+    #                                           success_action_status: 201,
+    #                                           acl: :bucket_owner_full_control).url
   end
 
 end
