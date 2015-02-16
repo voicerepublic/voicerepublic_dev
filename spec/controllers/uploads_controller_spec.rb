d8a1bf198f1356e0c996b74550d5bf26e589a959
phil <phil@branch14.org>
2015-02-16 11:22:04 +0100

    fix specs

--- spec/controllers/uploads_controller_spec.rb
+++ spec/controllers/uploads_controller_spec.rb
@@ -16,4 +16,12 @@ describe UploadsController do
     end
   end
 
+  describe 'Talk#upload' do
+    it 'creates a pre-signed S3 URL that will be used in Angular File Upload' do
+      get :new
+      assigns(:presigned_s3_post_url).should_not be_nil
+      assigns(:presigned_s3_post_url).to_s.should =~ /http.*s3.*/
+    end
+  end
+
 end
