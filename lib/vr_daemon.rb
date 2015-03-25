d06de64258c4da5a663350ad66a00ea645bdceae
phil <phil@branch14.org>
2015-03-25 13:26:25 +0100

    make event EndTalk work via faye

--- lib/vr_daemon.rb
+++ lib/vr_daemon.rb
@@ -1,17 +1,17 @@
 #!/usr/bin/env ruby
 
-# pull in the whole rails environment
-require File.expand_path('../../config/environment', __FILE__)
 require 'daemons'
 
 class VrDaemon
 
   CHANNEL = '/live/up'
 
+  attr_accessor :client
+
   def run
     extension = Faye::Authentication::ClientExtension.new(Settings.faye.secret_token)
     EM.run {
-      client = Faye::Client.new(Settings.faye.server)
+      self.client = Faye::Client.new(Settings.faye.server)
       client.add_extension(extension)
 
       puts "subcribing to #{CHANNEL}..."
@@ -22,7 +22,16 @@ class VrDaemon
   end
 
   def process(msg)
-    p msg
+    case msg['event']
+    when 'EndTalk'
+      _, talk_id, user_id = msg['channel'].match(%r{^/live/up/t(\d+)/u(\d+)$}).to_a
+      talk = Talk.find(talk_id)
+      # TODO authorize
+      talk.end_talk!
+      client.publish talk.public_channel, event: 'EndTalk'
+    else
+      puts "Don't know how to handle:\n#{msg.to_yaml}"
+    end
   end
 
 end
@@ -32,6 +41,10 @@ if __FILE__ == $0
   base = File.expand_path('../..', __FILE__)
   piddir = File.join(base, 'tmp', 'pids')
   Daemons.run_proc(File.basename(__FILE__), dir: piddir) do
+    Dir.chdir(base)
+    # pull in the whole rails environment
+    puts 'booting rails...'
+    require File.expand_path('config/environment', base)
     VrDaemon.new.run
   end
 end
