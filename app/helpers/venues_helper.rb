5142949499495e679d929c9c9ffe9d36f8c2fa70
phil <phil@branch14.org>
2015-04-03 13:23:19 +0200

    cleanup, remove obsolete helpers & partials

--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -1,20 +1,4 @@
-# TODO cleanup! is anything of this used?
 module VenuesHelper
-  def recording_path(recording)
-    "/system/recordings/#{recording}"
-  end
-
-  def end_in_milli(venue)
-    end_time = venue.start_time.utc + venue.duration.minutes
-    diff = end_time - Time.now.utc
-    diff < 0 ? 0 : diff * 1000.0
-  end
-
-  #TODO replace with cancan
-  def can_comment_venue?(venue)
-    return false unless user_signed_in?
-    venue.users.include?(current_user)
-  end
 
   def talks_partial(collection)
     collection.count > 1 ? 'shared/talk_small_box' : 'shared/talk_medium_box'
