8d37ba3ce6817e1c20e9af9680ece7de64ac77d6
phil <phil@branch14.org>
2015-04-08 14:56:50 +0200

    remove rake task which deleted guest users

--- config/schedule.rb
+++ config/schedule.rb
@@ -29,10 +29,6 @@ every 40.minutes, :roles => [:app] do
 end
 
 every 60.minutes, :roles => [:app] do
-  rake "cleanup:guests"
-end
-
-every 60.minutes, :roles => [:app] do
   rake "cleanup:fix_abandoned_talk_state"
 end
 
