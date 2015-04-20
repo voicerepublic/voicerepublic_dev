d32d8e581612e749bc75967916154756ec7c17cc
phil <phil@branch14.org>
2015-04-20 16:40:08 +0200

    announce new pricing

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,6 @@ Rails.application.routes.draw do
 
   resources :uploads, only: [ :new, :create ]
 
-  #get "/pricing", to: 'static_pages#pricing', as: 'static_pages_pricing'
-  get "/pricing", to: 'purchases#index', as: 'static_pages_pricing'
-
   post '/xhr/talk/:id/messages', to: 'xhr/messages#create'
   get  '/xhr/users',             to: 'xhr/users#index'
 
@@ -20,9 +17,12 @@ Rails.application.routes.draw do
   end
 
   if Settings.api.try(:enabled)
+    get "/pricing", to: 'purchases#index', as: 'static_pages_pricing'
     namespace 'api' do
       resources :talks, only: [:index]
     end
+  else
+    get "/pricing", to: 'static_pages#pricing', as: 'static_pages_pricing'
   end
 
   post '/search',              to: 'search#create'
