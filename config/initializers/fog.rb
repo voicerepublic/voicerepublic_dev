d388ca6a744588456e4f222e2db4c7df63cc0a41
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2015-05-01 16:10:49 +0200

    temporarily remove fog bucket creation

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -2,13 +2,13 @@ puts "[DBG] Initializing Fog..."
 
 Storage = Fog::Storage.new(Settings.fog.storage.to_hash)
 
-puts "[DBG] Creating buckets..."
-
-Storage.directories.create key: Settings.storage.media
-Storage.directories.create key: Settings.storage.import
-Storage.directories.create key: Settings.storage.uploads, acl: 'public-read-write'
-
-puts "[DBG] Creating buckets complete."
+#puts "[DBG] Creating buckets..."
+#
+#Storage.directories.create key: Settings.storage.media
+#Storage.directories.create key: Settings.storage.import
+#Storage.directories.create key: Settings.storage.uploads, acl: 'public-read-write'
+#
+#puts "[DBG] Creating buckets complete."
 
 # when using local storage provider, fake expiring urls with public
 # urls
