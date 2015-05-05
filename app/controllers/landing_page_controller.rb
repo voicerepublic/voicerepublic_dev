9d8c7295c430184e3a8fc1ba97f3b0af61dc306b
phil <phil@branch14.org>
2015-05-05 15:10:16 +0200

    show all live & halflive talks on landing page, increase number of others to 6

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -3,10 +3,11 @@ class LandingPageController < BaseController
   def index
     respond_to do |format|
       format.html do
-        @talks_featured = Talk.featured.limit(5)
-        @talks_live     = Talk.live_and_halflive.limit(5)
-        @talks_recent  	= Talk.recent.limit(5)
-        @talks_popular  = Talk.popular.limit(5)
+        @talks_live     = Talk.live_and_halflive
+
+        @talks_featured = Talk.featured.limit(6)
+        @talks_recent  	= Talk.recent.limit(6)
+        @talks_popular  = Talk.popular.limit(6)
       end
       format.rss do
         @podcast = OpenStruct.new(talks: Talk.recent.limit(10))
