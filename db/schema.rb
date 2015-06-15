2e006a887191bd66f42c5af812a0b6cebca843f5
Nick Niles <nick@voicerepublic.com>
2015-06-15 10:59:17 +0200

    add extensions to schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 20150502194739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
+  enable_extension "unaccent"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
