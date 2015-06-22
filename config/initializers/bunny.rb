c50557bc24986617f6d96e810d1a57c686cf7909
phil <phil@branch14.org>
2015-06-22 16:49:02 +0200

    deactivate notifications again

--- config/initializers/bunny.rb
+++ config/initializers/bunny.rb
@@ -48,77 +48,77 @@ BUNNY = BunnyWrapper.new
 BUNNY.publish(queue: 'log', pid: $$, state: 'Rails ready')
 
 
-# And now for something completely different...
-unless Rails.env.test?
-
-  # Subscribe to all of Rails' notifications and publish to an
-  # exchange on RabbitMQ. Publishing to an exchange means, that if
-  # nobody is listending RabbitMQ will discard these messages
-  # instantly. Much like it would be the case if we'd use Faye for it.
-  ActiveSupport::Notifications.subscribe(//) do |*args|
-    data = args.last
-
-    # visual should be a one line representation of the event
-    visual =
-      case args.first # name
-
-      # generic notifications
-      when 'sql.active_record'
-        "[%s] %s" % [ data[:name],
-                      data[:sql].split("\n").map(&:strip) * ' ' ]
-
-      when 'instantiation.active_record'
-        "%s * %s" % [ data[:record_count],
-                      data[:class_name] ]
-
-      when 'load_config_initializer.railties'
-        data[:initializer].sub(Rails.root.to_s, '')
-
-      when '!render_template.action_view',
-           '!compile_template.action_view'
-        data[:virtual_path]
-
-      when 'render_template.action_view'
-        data[:layout]
-
-      when 'render_partial.action_view'
-        data[:identifier].sub(Rails.root.to_s, '')
-
-      when 'render_collection.action_view'
-        "%s %s" % [ data[:identifier].sub(Rails.root.to_s, ''),
-                    data[:count] ]
-
-      when 'process_action.action_controller',
-           'start_processing.action_controller'
-        "%s %s %s %s %s#%s" % [ data[:method],
-                                data[:path],
-                                data[:format],
-                                data[:status],
-                                data[:controller],
-                                data[:action] ]
-
-      # custom notifications
-      when 'flyer.inkscape.vr'
-        data[:cmd]
-
-      when 'run_chain.audio_process.vr'
-        data[:chain] * ' '
-
-      # fall back to json for unknown events
-      else data.to_json
-      end
-
-    duration = (args[2] - args[1]) * 1000
-
-    params = [
-      # custom arguments
-      :exchange, :duration, :visual,
-      # standard arguments
-      :name, :started, :finished, :unique_id, :data
-    ]
-
-    # and send it to rabbitmq
-    BUNNY.publish(Hash[params.zip(args.unshift('metrics', duration, visual))])
-  end
-
-end
+# # And now for something completely different...
+# unless Rails.env.test?
+#
+#   # Subscribe to all of Rails' notifications and publish to an
+#   # exchange on RabbitMQ. Publishing to an exchange means, that if
+#   # nobody is listending RabbitMQ will discard these messages
+#   # instantly. Much like it would be the case if we'd use Faye for it.
+#   ActiveSupport::Notifications.subscribe(//) do |*args|
+#     data = args.last
+#
+#     # visual should be a one line representation of the event
+#     visual =
+#       case args.first # name
+#
+#       # generic notifications
+#       when 'sql.active_record'
+#         "[%s] %s" % [ data[:name],
+#                       data[:sql].split("\n").map(&:strip) * ' ' ]
+#
+#       when 'instantiation.active_record'
+#         "%s * %s" % [ data[:record_count],
+#                       data[:class_name] ]
+#
+#       when 'load_config_initializer.railties'
+#         data[:initializer].sub(Rails.root.to_s, '')
+#
+#       when '!render_template.action_view',
+#            '!compile_template.action_view'
+#         data[:virtual_path]
+#
+#       when 'render_template.action_view'
+#         data[:layout]
+#
+#       when 'render_partial.action_view'
+#         data[:identifier].sub(Rails.root.to_s, '')
+#
+#       when 'render_collection.action_view'
+#         "%s %s" % [ data[:identifier].sub(Rails.root.to_s, ''),
+#                     data[:count] ]
+#
+#       when 'process_action.action_controller',
+#            'start_processing.action_controller'
+#         "%s %s %s %s %s#%s" % [ data[:method],
+#                                 data[:path],
+#                                 data[:format],
+#                                 data[:status],
+#                                 data[:controller],
+#                                 data[:action] ]
+#
+#       # custom notifications
+#       when 'flyer.inkscape.vr'
+#         data[:cmd]
+#
+#       when 'run_chain.audio_process.vr'
+#         data[:chain] * ' '
+#
+#       # fall back to json for unknown events
+#       else data.to_json
+#       end
+#
+#     duration = (args[2] - args[1]) * 1000
+#
+#     params = [
+#       # custom arguments
+#       :exchange, :duration, :visual,
+#       # standard arguments
+#       :name, :started, :finished, :unique_id, :data
+#     ]
+#
+#     # and send it to rabbitmq
+#     BUNNY.publish(Hash[params.zip(args.unshift('metrics', duration, visual))])
+#   end
+#
+# end
