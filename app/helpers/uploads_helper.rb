c8564aa0fd12a304b56a51094fa5fb0b05dc6d58
phil <phil@branch14.org>
2015-06-22 14:10:00 +0200

    refactor init of uploader into helper

---
+++ app/helpers/uploads_helper.rb
@@ -0,0 +1,18 @@
+module UploadsHelper
+
+  def init_audio_uploader
+    params = {
+      uploadUrl: @presigned_s3_post_url,
+      key:       SecureRandom.uuid
+      filter:    %w( ogg x-ogg
+                     wav x-wav wave x-pn-wav
+                     m4a x-m4a
+                     mp3 x-mp3 mpeg3 x-mpeg3
+                     mpg x-mpegaudio mpeg ) * ' ',
+    }
+    "init(#{params.to_json})"
+  end
+
+
+
+end
