0b35e7c7ba8c8b632330e95232562655c3912885
phil <phil@branch14.org>
2015-07-06 17:34:18 +0200

    i deleted some code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,10 +34,6 @@ module ApplicationHelper
     arr.length > num-1 ? arr[0..num-1].join(" ").concat(" ...") : txt
   end
 
-  def simple_links(txt)
-    txt.gsub(/(https?:\/\/\S*)/, "<a href='\\1' target='_blank'>\\1</a>")
-  end
-
   # adds iframes for typical youtube links
   #
   # e.g.
@@ -66,25 +62,6 @@ module ApplicationHelper
     end
 
     txt
-  end
-
-  # simple_format, but with simple links target blanks preserved
-  # TODO replace with somethink like https://github.com/jch/html-pipeline
-  def sophisticated_format(txt)
-    simple_format(youtubify(simple_links(sanitize(txt))), {}, :sanitize => false)
-  end
-
-  def app_mode
-    Rails.env
-  end
-
-  def git_revision
-    gi = KluuuCode::GitInfo.new(Rails.root)
-    gi.latest
-  end
-
-  def klu_type_string(klu)
-    klu.instance_of?(Kluuu) ? t('helper.application.kluuu_string') : t('helper.application.no_kluuu_string')
   end
 
   # TODO: move into trickery
--- config/initializers/set_local_ip.rb
+++
@@ -1,5 +0,0 @@
-require 'kluuu_code/helper'
-
-VoiceRepublic::Application.config.ip_address = KluuuCode::Helper.local_ip
-VoiceRepublic::Application.config.ip_port = KluuuCode::Helper.local_port
-
