fa5d66abb435b20fc166a8a0d49713457d337c22
phil <phil@branch14.org>
2015-07-07 15:22:09 +0200

    bring back youtubify as an extension to the redcarpet renderer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,46 +42,6 @@ module ApplicationHelper
     arr.length > num-1 ? arr[0..num-1].join(" ").concat(" ...") : txt
   end
 
-  def simple_links(txt)
-    txt.gsub(/(https?:\/\/\S*)/, "<a href='\\1' target='_blank'>\\1</a>")
-  end
-
-  # adds iframes for typical youtube links
-  #
-  # e.g.
-  #   http://www.youtube.com/watch?v=F0G0YNHINwY
-  #   or
-  #   http://youtu.be/F0G0YNHINwY
-  #
-  # will be added as
-  #   <iframe width="560" height="315" frameborder="0" allowfullscreen
-  #     src="//www.youtube.com/embed/F0G0YNHINwY"></iframe>
-  #
-  def youtubify(txt)
-    template = "\n\n<iframe width='640' height='480' " +
-      "src='//www.youtube.com/embed/%s' " +
-      "frameborder='0' allowfullscreen></iframe>"
-
-    patterns = [ /https?:\/\/www\.youtube\.com\/watch\?v=(\S+)/,
-                 /https?:\/\/youtu\.be\/(\S+)/ ]
-
-    patterns.map do |pattern|
-      if md = txt.match(pattern)
-        md.to_a[1..-1].each do |key|
-          txt += template % key
-        end
-      end
-    end
-
-    txt
-  end
-
-  # simple_format, but with simple links target blanks preserved
-  # TODO replace with somethink like https://github.com/jch/html-pipeline
-  def sophisticated_format(txt)
-    simple_format(youtubify(simple_links(sanitize(txt))), {}, :sanitize => false)
-  end
-
   def app_mode
     Rails.env
   end
