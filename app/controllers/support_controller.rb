3f08e56a81a8f8ee4811ceba7c9a6b4b6b643b4e
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2015-07-09 19:24:01 +0200

    using File.read

--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -5,7 +5,7 @@ class SupportController < ApplicationController
   def api
     @api_doc = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new,
                                        fenced_code_blocks: true).
-                                       render(File.open(Rails.root.join("doc/API.md"), "r").read)
+                                       render(File.read(Rails.root.join("doc/API.md")))
   end
 
 end
