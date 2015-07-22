ca71e6790f8199e6127940577cc183d6583703e4
phil <phil@branch14.org>
2015-07-22 11:29:18 +0200

    no dryrun talks on prolific controllers (landing_page & explore)

--- spec/controllers/landing_page_controller_spec.rb
+++ spec/controllers/landing_page_controller_spec.rb
@@ -12,6 +12,15 @@ describe LandingPageController do
       get 'index', format: 'rss'
       expect(response).to be_success
     end
+
+    it 'should not show dryrun talks among live' do
+      public_talk = FactoryGirl.create(:talk, state: 'live')
+      private_talk = FactoryGirl.create(:talk, dryrun: true, state: 'live')
+      get :index
+      talks_live = assigns(:talks_live)
+      expect(talks_live).to include(public_talk)
+      expect(talks_live).not_to include(private_talk)
+    end
   end
-  
+
 end
