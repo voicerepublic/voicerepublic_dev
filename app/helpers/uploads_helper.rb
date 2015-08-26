58f1a365a1e3938f608b550a663c90394925921f
phil <phil@branch14.org>
2015-08-26 10:59:30 +0200

    refactored uploads helper

--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -1,41 +1,32 @@
 module UploadsHelper
 
   def init_audio_uploader
-    key = SecureRandom.uuid
-
-    bucket = Settings.storage.upload_audio
-    object_path = ''
-    expires = 1.day.from_now
-
-    params = {
-      uploadUrl: Storage.put_object_url(bucket, object_path, expires),
-      key:       key,
-      filter:    %w( ogg x-ogg
-                     wav x-wav wave x-pn-wav
-                     m4a x-m4a
-                     mp3 x-mp3 mpeg3 x-mpeg3
-                     mpg x-mpegaudio mpeg ) * ' ',
-      # `success` will be evaled on complete
-      success:   "$('#talk_user_override_uuid').attr('value', '#{key}')",
-      safetynetMessage: I18n.t('talks.fields.unprocessed_upload')
-    }
-
-    "init(#{params.to_json})"
+    init_uploader Settings.storage.upload_audio,
+                  %w( ogg x-ogg
+                      wav x-wav wave x-pn-wav
+                      m4a x-m4a
+                      mp3 x-mp3 mpeg3 x-mpeg3
+                      mpg x-mpegaudio mpeg ) * ' ',
+                  "$('#talk_user_override_uuid').attr('value', '%s')"
   end
 
   def init_slides_uploader
+    init_uploader Settings.storage.upload_slides,
+                  'pdf',
+                  "$('#talk_slides_uuid').attr('value', '%s')"
+  end
+
+  private
+
+  def init_uploader(bucket, filter, success_tmpl)
     key = SecureRandom.uuid
 
-    bucket = Settings.storage.upload_slides
-    object_path = ''
-    expires = 1.day.from_now
-
     params = {
-      uploadUrl: Storage.put_object_url(bucket, object_path, expires),
+      uploadUrl: Storage.put_object_url(bucket, '', 1.day.from_now),
       key:       key,
-      filter:    'pdf',
+      filter:    filter,
       # `success` will be evaled on complete
-      success:   "$('#talk_slides_uuid').attr('value', '#{key}')",
+      success:   success_tmpl % key,
       safetynetMessage: I18n.t('talks.fields.unprocessed_upload')
     }
 
