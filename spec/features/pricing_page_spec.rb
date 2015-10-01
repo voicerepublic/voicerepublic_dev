3c53a9254aa718d4d2f5b1e828a136d9764724e6
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2015-10-01 19:01:11 +0200

    add price per talk plus fix specs

--- spec/features/pricing_page_spec.rb
+++ spec/features/pricing_page_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe 'PricingPage' do
   it 'renders' do
-  	visit root_path
-  	within 'footer' do
-  		click_on 'Pricing'
-  	end
-    expect(page).to have_content ("Enterprise")
+    visit root_path
+    within 'footer' do
+      click_on 'Pricing'
+    end
+    expect(page).to have_content ("Talk Credit Packages")
   end
 end
