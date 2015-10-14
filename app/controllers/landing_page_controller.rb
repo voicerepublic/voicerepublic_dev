2504c1485df17667207779e2863f5411c61c8541
Nick Niles <nick@voicerepublic.com>
2015-10-14 18:27:47 +0200

    create velvet layout and begin scaffolding new landing page

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,5 +1,6 @@
 class LandingPageController < BaseController
 
+  layout 'velvet'
   def index
     respond_to do |format|
       format.html do
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( support.css support.js )
+Rails.application.config.assets.precompile += %w( support.css support.js velvet.css )
