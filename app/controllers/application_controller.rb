ae9c4477f6e43edee3dda68122cb7112ce189d53
Nick Niles <nick@voicerepublic.com>
2015-10-21 12:27:40 +0200

    velvet application layout and set main application layout to velvet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+  layout 'application_velvet'
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
