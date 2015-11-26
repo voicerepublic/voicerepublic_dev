1fdef68ddae2eb3e07ecda07709cd2cc7d53aff5
phil <phil@branch14.org>
2015-11-26 17:24:13 +0100

    fix missed category

--- app/controllers/xhr/tags_controller.rb
+++ app/controllers/xhr/tags_controller.rb
@@ -3,7 +3,7 @@ class Xhr::TagsController < Xhr::BaseController
   def index
     scope = ActsAsTaggableOn::Tag.
             where(["name ILIKE ?", "%#{params[:q]}%"]).
-            order('category DESC', :name)
+            order('promoted DESC', :name)
 
     tags = scope.paginate(:page => params[:page], :per_page => params[:limit] || 10)
     render json: { tags: tags, total: scope.count }
