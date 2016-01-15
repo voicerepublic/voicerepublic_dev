1fe8e0c4ddbde901325f3816f70c29748a7534ee
Nick Niles <nick@voicerepublic.com>
2016-01-15 11:31:05 +0100

    make explore page more responsive

--- public/assets/javascripts/explore.js
+++ public/assets/javascripts/explore.js
@@ -3,3 +3,7 @@ $(".category-buttons input[type='radio']").on('click', function(){
     $(this).parent().addClass('selected');
 });
 
+$(".category-buttons input[type='radio']").focus(function(){     
+    $('.category-buttons label').removeClass('selected');
+    $(this).parent().addClass('selected');
+});
