4987de80e35aa545d9b03b3d556aeb1e09357cca
phil <phil@branch14.org>
2016-01-15 11:21:04 +0100

    read errbit host from settings

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,7 @@
 if Settings.errbit.enabled
   Airbrake.configure do |config|
     config.api_key = Settings.errbit.api_key
-    config.host    = 'voicerepublic-errbit.herokuapp.com'
+    config.host    = Settings.errbit.host
     config.port    = 80
     config.secure  = config.port == 443
   end
