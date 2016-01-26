a0a9d71c32de3d42c91ad139c9ef39d66fb84fec
phil <phil@branch14.org>
2016-01-26 21:51:42 +0100

    one more mailer preview

--- spec/mailers/previews/custom_devise_mailer_preview.rb
+++ spec/mailers/previews/custom_devise_mailer_preview.rb
@@ -8,4 +8,18 @@ class CustomDeviseMailerPreview < ActionMailer::Preview
     ::CustomDeviseMailer.confirmation_instructions(User.last, 'faketoken', {})
   end
 
+  def reset_password_instructions
+    ::CustomDeviseMailer.reset_password_instructions(User.last, 'faketoken', {})
+  end
+
+  # We don't use that feature
+  #def unlock_instructions
+  #  ::CustomDeviseMailer.unlock_instructions(User.last, 'faketoken', {})
+  #end
+
+  # We have to upgrade devise to use that feature
+  #def password_change
+  #  ::CustomDeviseMailer.password_change(User.last, {})
+  #end
+
 end
