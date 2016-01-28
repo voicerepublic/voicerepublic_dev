d785f5f1f441a7c6244e44f37143c7ce82de2fca
Nick Niles <nick@voicerepublic.com>
2016-01-28 16:54:19 +0100

    responsive serach

--- public/assets/javascripts/app.js
+++ public/assets/javascripts/app.js
@@ -22,7 +22,10 @@ $('.flash-msg').delay(3000).fadeOut(1000);
 //     });
 // });
 
-
+$('#mobile-logo').on('click', function(){
+    console.log('opening the mobile nav');
+    $('#mobile-nav').toggle();
+});
 
 $('.button-play-pause').on('click', function() {
     $(this).children('span').each(function() {
