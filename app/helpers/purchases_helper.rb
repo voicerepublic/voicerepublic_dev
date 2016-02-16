790cca6a4ce57055ecf26d9745f76fdffc37f62f
Nick Niles <nick@voicerepublic.com>
2016-02-16 14:39:30 +0100

    change paypal btn again, and replace new text with even new text. don't ask

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -3,7 +3,7 @@ module PurchasesHelper
   include Pricing
 
   def paypal_button_url
-    'https://www.paypalobjects.com/webstatic/en_US/btn/btn_checkout_pp_142x27.png'
+    'https://www.paypalobjects.com/webstatic/en_US/btn/btn_buynow_107x26.png'
   end
 
 end
