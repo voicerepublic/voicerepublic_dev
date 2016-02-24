e2331272d1715ad97d417c2438e34c01dfba44d5
phil <phil@branch14.org>
2016-02-24 15:56:02 +0100

    fix 2 specs

--- spec/features/participations_spec.rb
+++ spec/features/participations_spec.rb
@@ -7,25 +7,17 @@ describe "Participations" do
       @user = FactoryGirl.create(:user)
       login_user @user
     end
-    # TODO: Participation used to be explicit. We need to rewrite it to be
-    # implicit. When done, we should re-activate this spec.
-    skip "creates a participation from Talk" do
-      expect {
-        visit talk_path(@talk)
-        click_on "Participate"
-      }.to change(Participation, :count).by(1)
-    end
-    it "creates a participation from Series" do
+    it "creates a participation from Series", js: true do
       expect {
         visit series_path(@talk.series)
-        click_on "Subscribe to Series"
+        find('.qa-subscribe').click
       }.to change(Participation, :count).by(1)
     end
-    it "deletes a participation from Series" do
+    it "deletes a participation from Series", js: true do
       FactoryGirl.create(:participation, series: @talk.series, user: @user)
       expect {
         visit series_path(@talk.series)
-        click_on "Unsubscribe"
+        find('.qa-unsubscribe').click
       }.to change(Participation, :count).by(-1)
       expect(current_path).to eq(series_path(@talk.series))
     end
