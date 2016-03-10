19a0260a262f3eab7e9834678f20bcd1d299c3a1
Nick Niles <nick@voicerepublic.com>
2016-03-10 16:00:30 +0100

    lots of structure and style changes to embed

--- public/assets/javascripts/embed.js
+++ public/assets/javascripts/embed.js
@@ -17,7 +17,50 @@ $(function () {
       supplied: "m4a, mp3, ogg",
       wmode: "window",
       smoothPlayBar: true,
-      keyEnabled: true
+      keyEnabled: true,
+      verticalVolume: true
     });
   });
+
+  var embedWidth;
+  var player = $('.embed-player');
+
+  function whatWidth(){
+    embedWidth = $('.embed-player').outerWidth();
+    $('.my-width').html(embedWidth)
+    
+    if (embedWidth < 480) {
+      player.removeClass('medium')
+      player.removeClass('large')
+      player.removeClass('small')
+      player.addClass('xs')
+      $('.sm-up').hide();
+      $('.md-up').hide();
+
+    } else if (embedWidth > 479 && embedWidth < 640) {
+      player.removeClass('medium')
+      player.removeClass('large')
+      player.removeClass('xs')
+      player.addClass('small')
+      $('.sm-up').show();
+      $('.md-up').hide();
+    } else if (embedWidth > 639 && embedWidth < 1024) {
+      player.removeClass('small')
+      player.removeClass('large')
+      player.removeClass('xs')
+      player.addClass('medium')
+      $('.md-up').show();
+    } else {
+      player.removeClass('medium')
+      player.removeClass('small')
+      player.removeClass('xs')
+      player.addClass('large')
+      $('.sm-up').show();
+      $('.md-up').show();
+    }
+  }
+  
+  whatWidth();
+  $(window).resize(whatWidth)
+
 });
