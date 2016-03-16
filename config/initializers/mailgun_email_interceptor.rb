88c889a8f2fbe28568215a748fc89157739dc922
Lino von Burg <linovonburg@gmail.com>
2016-03-16 14:40:18 +0100

    fixed adding header

--- config/initializers/mailgun_email_interceptor.rb
+++ config/initializers/mailgun_email_interceptor.rb
@@ -1,6 +1,9 @@
+# We need to add the header 'x-mailgun-native-send' to mails that go to voicerepublic.com addresses,
+# otherwise it will fail atmails sender verification.
+
 class MailgunEmailInterceptor
   def self.delivering_email(message)
-    message['x-mailgun-native-send'] = true if message.to =~ /^.*@voicerepublic.com$/
+     message.mailgun_headers = {'x-mailgun-native-send' => 'true'} if message.to.first =~ /@voicerepublic.com$/
   end
 end
 
