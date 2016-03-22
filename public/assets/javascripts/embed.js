2f0657dc87d677a5f879b5cd4e4feb045b44f9be
Nick Niles <nick@voicerepublic.com>
2016-03-22 15:56:14 +0100

    add more responsive sizes and behaviors, add GA tracking to end of media

--- public/assets/javascripts/embed.js
+++ public/assets/javascripts/embed.js
@@ -1,178 +1,208 @@
 // http://jplayer.org/latest/developer-guide/
 
-$(function () {
-  $(".vr-player.jp-jplayer").each(function (i, el) {
-    var player = $(el)
-    var volumeIndicator = player.next().find('.jp-volume-bar-value');
-    var dancingIndicator = player.next().find('.jp-volume-bar-value-dancing');
-    var interval = null;
-    var state = "started";
+$(function() {
+    $(".vr-player.jp-jplayer").each(function(i, el) {
+        var player = $(el)
+        var volumeIndicator = player.next().find('.jp-volume-bar-value');
+        var dancingIndicator = player.next().find('.jp-volume-bar-value-dancing');
+        var interval = null;
+        var state = "started";
 
-    var start_volume_meter = function() {
-      if (state == "playing") {
-        var currentVolumeHeight = volumeIndicator.css("height")
-        interval = setInterval(dancingVolume, 100, currentVolumeHeight, dancingIndicator);
-      } else {
-        dancingIndicator.css("height", volumeIndicator.height());
-      }
-    }
+        var start_volume_meter = function() {
+            if (state == "playing") {
+                var currentVolumeHeight = volumeIndicator.css("height")
+                interval = setInterval(dancingVolume, 100, currentVolumeHeight, dancingIndicator);
+            } else {
+                dancingIndicator.css("height", volumeIndicator.height());
+            }
+        }
 
-    player.jPlayer({
-      ready: function (event) {
-        $(this).jPlayer("setMedia", {
-          m4a: player.data('m4a'),
-          mp3: player.data('mp3'),
-          ogg: player.data('ogg')
+        player.jPlayer({
+            ready: function(event) {
+                $(this).jPlayer("setMedia", {
+                    m4a: player.data('m4a'),
+                    mp3: player.data('mp3'),
+                    ogg: player.data('ogg')
+                });
+            },
+
+            playing: function(event) {
+                var currentVolumeHeight = volumeIndicator.css("height")
+                state = "playing";
+                start_volume_meter();
+            },
+
+            pause: function(event) {
+                state = "paused";
+                clearInterval(interval);
+            },
+
+            volumechange: function(event) {
+                clearInterval(interval);
+                start_volume_meter();
+            },
+
+            ended: function() {
+                toggleTeaser();
+                _gaq.push("_trackEvent", "click", "play", "player", "ended", "talk: " + talk_id)
+            },
+
+            volume: .8,
+            cssSelectorAncestor: player.data('selector'),
+            swfPath: '/flash/Jplayer.swf',
+            supplied: "m4a, mp3, ogg",
+            wmode: "window",
+            smoothPlayBar: true,
+            keyEnabled: true,
+            verticalVolume: true
+
         });
-      },
 
-      playing: function (event) {
-        var currentVolumeHeight = volumeIndicator.css("height")
-        state = "playing";
-        start_volume_meter();
-      }, 
-
-      pause: function (event) {
-       state = "paused";
-       clearInterval(interval);
-      },
-
-      volumechange: function(event) {
-        clearInterval(interval);
-        start_volume_meter();
-      },
-
-      ended: function(){
-        toggleTeaser();
-        _gaq.push("_trackEvent", "click","play", "player","ended", "talk: "+ talk_id)        
-      },
-
-      volume: .8,
-      cssSelectorAncestor: player.data('selector'),
-      swfPath: '/flash/Jplayer.swf',
-      supplied: "m4a, mp3, ogg",
-      wmode: "window",
-      smoothPlayBar: true,
-      keyEnabled: true,
-      verticalVolume: true
+        //hide the teaser again:
+        $('.replay-btn').on('click', function() {
+            toggleTeaser();
+            player.jPlayer("play");
+        });
 
     });
 
-//hide the teaser again:
-$('.replay-btn').on('click', function(){
-  toggleTeaser();
-  player.jPlayer("play");
-});
-    
-  });
-
-function toggleTeaser() {
-  $('.player-box').toggleClass('hide');
-  $('.teaser-msg').toggleClass('hide');
-}
-
-
-
-  function dancingVolume(j,v) {
-      var rando = Math.random() * .5;
-      var height = parseInt(j, 10);
-      var origHeight = height + "px";
-      var newHeight = height - (rando * height)  + "px";
-    if ($('.jp-audio').hasClass('jp-state-playing')) {
-      $('.jp-state-playing').find(v).css("height",newHeight)
-    } else {
-      $('.jp-state-playing').find(v).css("height",origHeight)
-    }
-  }
-
-
-//Share and action btns functionality: 
-$('.action-share').on('click', function(){
-  $('.share-panel').toggleClass('hide');
-  $('.action-panel').toggleClass('hide');
-});
-
-
-$('.close-share').on('click',function(){
-    $('.share-panel').toggleClass('hide');
-    $('.action-panel').toggleClass('hide');
-});
-
-$('.close-msg').on('click',function(){
-    $('.embed-msg').toggleClass('hide');
-    $('.action-panel').toggleClass('hide');
-    $('.jp-progress').toggleClass('hide');
-
-});
-
-
-$('.pin-btn').on('click', function(){
-  $('.embed-msg').toggleClass('hide');
-  $('.action-panel').toggleClass('hide');
-  $('.jp-progress').toggleClass('hide');
-
-});
-
-//easter egg to make the player unbranded: 
-$('.image-box').on('click',function(){
-  $('.embed-player').toggleClass('unbranded');
-});
-  // Make the player responsive by checking the width of the element rather than of the browser window (since it's in an iframe)
-  var embedWidth;
-  var embedClass = "";
-  var playerHolder = $('.embed-player');
-
-  function showEmbedClass() {
-    $('.my-class').text("my-class: " + embedClass);
-  }
-
-  function whatWidth(){
-    embedWidth = $('.embed-player').outerWidth();
-    $('.my-width').html(embedWidth)
-    
-    if (embedWidth < 480) {
-      embedClass = "XS";
-      playerHolder.removeClass('medium')
-      playerHolder.removeClass('large')
-      playerHolder.removeClass('small')
-      playerHolder.addClass('xs')
-      $('.sm-up').hide();
-      $('.md-up').hide();
-
-    } else if (embedWidth > 479 && embedWidth < 640) {
-      embedClass = "Small";
-      playerHolder.removeClass('medium')
-      playerHolder.removeClass('large')
-      playerHolder.removeClass('xs')
-      playerHolder.addClass('small')
-      $('.sm-up').show();
-      $('.md-up').hide();
-    } else if (embedWidth > 639 && embedWidth < 1024) {
-      embedClass = "Medium";
-      playerHolder.removeClass('small')
-      playerHolder.removeClass('large')
-      playerHolder.removeClass('xs')
-      playerHolder.addClass('medium')
-      $('.md-up').show();
-    } else {
-      embedClass = "Large";
-      playerHolder.removeClass('medium')
-      playerHolder.removeClass('small')
-      playerHolder.removeClass('xs')
-      playerHolder.addClass('large')
-      $('.sm-up').show();
-      $('.md-up').show();
+    function toggleTeaser() {
+        $('.player-box').toggleClass('hide');
+        $('.teaser-msg').toggleClass('hide');
     }
 
-    showEmbedClass();
-  }
-  
-  whatWidth();
-  $(window).resize(whatWidth)
+
+
+    function dancingVolume(j, v) {
+        var rando = Math.random() * .5;
+        var height = parseInt(j, 10);
+        var origHeight = height + "px";
+        var newHeight = height - (rando * height) + "px";
+        if ($('.jp-audio').hasClass('jp-state-playing')) {
+            $('.jp-state-playing').find(v).css("height", newHeight)
+        } else {
+            $('.jp-state-playing').find(v).css("height", origHeight)
+        }
+    }
+
+
+    //Share and action btns functionality: 
+    $('.action-share').on('click', function() {
+        $('.share-panel').toggleClass('hide');
+        $('.action-panel').toggleClass('hide');
+        $('.jp-time-holder').addClass('sharing');
+    });
+
+
+    $('.close-share').on('click', function() {
+        $('.share-panel').toggleClass('hide');
+        $('.action-panel').toggleClass('hide');
+        $('.jp-time-holder').removeClass('sharing');
+    });
+
+    $('.close-msg').on('click', function() {
+        $('.embed-msg').toggleClass('hide');
+        $('.action-panel').toggleClass('hide');
+        $('.jp-progress').toggleClass('hide');
+
+    });
+
+
+    $('.pin-btn').on('click', function() {
+        $('.embed-msg').toggleClass('hide');
+        $('.action-panel').toggleClass('hide');
+        $('.jp-progress').toggleClass('hide');
+
+    });
+
+    //easter egg to make the player unbranded: 
+    $('.image-box').on('click', function() {
+        $('.embed-player').toggleClass('unbranded');
+    });
+
+    // Make the player responsive by checking the width of the element rather than of the browser window (since it's in an iframe)
+    var embedWidth;
+    var embedClass = "";
+    var playerHolder = $('.embed-player');
+
+    function showEmbedClass() {
+        $('.my-class').text("my-class: " + embedClass);
+    }
+
+    //breakpoints: 
+    var token = 150;
+    var tiny = 320;
+    var xs = 480;
+    var small = 640;
+    var medium = 1024;
+
+    var breakpoints = [
+        "token",
+        "tiny",
+        "xs",
+        "small",
+        "medium",
+        "large"
+    ];
+
+    var hideForTiny = [
+        $('.slides-btn'),
+        $('.chat-btn'),
+        $('.action-share'),
+    ];
+
+    var addBreakpointClass = function(c) {
+        jQuery.each(breakpoints, function(index, item) {
+            playerHolder.removeClass(item)
+        });
+
+        if (c == "tiny" || c== "token"){
+          
+        jQuery.each(hideForTiny, function() {
+            $(this).addClass('hide')
+        });
+        } else {
+                  jQuery.each(hideForTiny, function() {
+            $(this).removeClass('hide')
+        });
+        }
+
+        playerHolder.addClass(c)
+        $('.mySize').text(c)
+
+    };
+
+    function calcWidth() {
+        embedWidth = $('.embed-player').outerWidth();
+        $('.my-width').html(embedWidth)
+        if (embedWidth <= token) {
+            embedClass = String(breakpoints[0])
+        } else if (embedWidth > token && embedWidth <= tiny) {
+            embedClass = String(breakpoints[1])
+            $('.sm-up').hide();
+            $('.md-up').hide();
+        } else if (embedWidth > tiny && embedWidth < xs) {
+            embedClass = String(breakpoints[2])
+            $('.sm-up').hide();
+            $('.md-up').hide();
+        } else if (embedWidth > xs && embedWidth < small) {
+            embedClass = String(breakpoints[3])
+            $('.sm-up').show();
+            $('.md-up').hide();
+        } else if (embedWidth > small && embedWidth < medium) {
+            embedClass = String(breakpoints[4])
+            $('.md-up').show();
+        } else {
+            embedClass = String(breakpoints[5])
+            $('.sm-up').show();
+            $('.md-up').show();
+        }
+        addBreakpointClass(embedClass);
+    }
+    calcWidth();
+    $(window).resize(calcWidth)
 
 
 
 
 });
-
-
