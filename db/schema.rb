c36bd5dbb6e6ca41984fd2d6a305f193ab6c415c
phil <phil@branch14.org>
2016-03-28 11:14:53 +0200

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319195803) do
+ActiveRecord::Schema.define(version: 20160319213905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,16 @@ ActiveRecord::Schema.define(version: 20160319195803) do
   end
 
   add_index "devices", ["organization_id"], name: "index_devices_on_organization_id", using: :btree
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "organization_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "memberships", ["organization_id"], name: "index_memberships_on_organization_id", using: :btree
+  add_index "memberships", ["user_id"], name: "index_memberships_on_user_id", using: :btree
 
   create_table "messages", force: :cascade do |t|
     t.integer  "user_id"
@@ -434,6 +444,8 @@ ActiveRecord::Schema.define(version: 20160319195803) do
   add_index "venues", ["user_id"], name: "index_venues_on_user_id", using: :btree
 
   add_foreign_key "devices", "organizations"
+  add_foreign_key "memberships", "organizations"
+  add_foreign_key "memberships", "users"
   add_foreign_key "talks", "venues"
   add_foreign_key "venues", "users"
 end
