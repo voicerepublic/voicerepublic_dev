19b2273b85196d835468608e9e1d54deb602eda4
Lino von Burg <linovonburg@gmail.com>
2016-04-11 15:10:03 +0200

    added environment for errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -3,6 +3,7 @@ if Settings.errbit.enabled
     config.project_key = Settings.errbit.api_key
     config.project_id = true
     config.host = Settings.errbit.host
+    config.ignore_environments = %w(development test)
     config.environment = Settings.errbit.environment
   end
 end
