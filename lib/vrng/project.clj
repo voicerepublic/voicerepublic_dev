9410c77006d35155d7aa24702e9fbbcdc420ec0e
phil <phil@branch14.org>
2016-04-11 10:47:14 +0200

    cleanup

--- lib/vrng/project.clj
+++ lib/vrng/project.clj
@@ -1,8 +1,8 @@
 (defproject vrng "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/munen/voicerepublic_dev"
+  :license {:name "proprietary"
+            :url "http://voicerepublic.com"}
 
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.7.228" :scope "provided"]
