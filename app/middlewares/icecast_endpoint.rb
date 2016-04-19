06bdaf718165c5f8cae9adbe9f6683e472fbdc98
phil <phil@branch14.org>
2016-04-19 13:07:23 +0200

    remove error ctahcing from icep

--- app/middlewares/icecast_endpoint.rb
+++ app/middlewares/icecast_endpoint.rb
@@ -27,8 +27,8 @@ class IcecastEndpoint < Struct.new(:app, :opts)
     end
 
     [ 200, {}, [] ]
-  rescue => e
-    return [ 409, {}, [e.message] ]
+  #rescue => e
+  #  return [ 409, {}, [e.message] ]
   end
 
 end
