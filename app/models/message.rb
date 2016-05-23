c2376e231bde85a43f6e411a1cde68a554ac6ba1
phil <phil@branch14.org>
2016-05-23 11:50:36 +0200

    properly populate alt tags on talks#show

--- app/models/message.rb
+++ app/models/message.rb
@@ -33,7 +33,8 @@ class Message < ActiveRecord::Base
       created_at: created_at,
       # extended
       user_name: user.name,
-      user_image_url: user.avatar.thumb('42x42').url
+      user_image_url: user.avatar.thumb('42x42#').url,
+      user_image_alt: user.image_alt
     }
   end
 
