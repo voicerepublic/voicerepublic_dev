0b043c5d211c8801125b1494ba7974aebf88f910
phil <phil@branch14.org>
2016-06-06 11:20:23 +0200

    fix typo

--- app/middlewares/php_responder.rb
+++ app/middlewares/php_responder.rb
@@ -3,7 +3,7 @@ class PhpResponder < Struct.new(:app, :opts)
   RESPONSE = [721, {}, ['721 - PHP, see https://github.com/joho/7XX-rfc']]
 
   def call(env)
-    return RESPONSE if env['PATH_INFO'].match(%{\.php(\?|#|$)})
+    return RESPONSE if env['PATH_INFO'].match(%r{\.php(\?|#|$)})
     app.call(env)
   end
 
