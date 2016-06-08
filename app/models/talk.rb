393903c9701569be4a21af1efd8c92f60312e475
phil <phil@branch14.org>
2016-06-08 11:05:16 +0200

    make the duration of an archived talk show up right away

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -445,6 +445,7 @@ class Talk < ActiveRecord::Base
         duration: duration,
 
         # extended
+        archived_duration: podcast_file[:seconds],
         flyer_path: flyer.path,
         embed_url: embed_self_url,
         media_links: media_links,
