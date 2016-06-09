caa9a64d3b9a904d4cd4fa6917c0ee4620950236
phil <phil@branch14.org>
2016-06-09 15:31:55 +0200

    fix error code of php responder

--- app/middlewares/php_responder.rb
+++ app/middlewares/php_responder.rb
@@ -1,6 +1,6 @@
 class PhpResponder < Struct.new(:app, :opts)
 
-  RESPONSE = [721, {}, ['721 - PHP, see https://github.com/joho/7XX-rfc']]
+  RESPONSE = [710, {}, ['710 - PHP, see https://github.com/joho/7XX-rfc']]
 
   def call(env)
     return RESPONSE if env['PATH_INFO'].match(%{\.php(\?|#|$)})
