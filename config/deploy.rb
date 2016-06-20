9671c21b6891b1bf984dd671b780adbbdf60b7e0
phil <phil@branch14.org>
2016-06-20 15:26:33 +0200

    properly symlink versions.edn

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,8 @@ set :deploy_to, '/home/app/app'
 # Default value for :linked_files is []
 set :linked_files, %w{ config/database.yml
                        config/bumpy_bridge.yml
-                       config/settings.local.yml }
+                       config/settings.local.yml
+                       config/versions.edn }
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache
