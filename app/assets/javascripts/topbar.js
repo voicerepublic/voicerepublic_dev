574a776a01e4e59b6c1d0500741fba6cd223fa31
Nick Niles <nick@voicerepublic.com>
2016-07-05 12:30:12 +0200

    give search-container display none when it's offscreen

--- app/assets/javascripts/topbar.js
+++ app/assets/javascripts/topbar.js
@@ -2,16 +2,16 @@ var $searchLoupe = $('#search-loupe a')
 var $searchContainer = $('.search-container')
 var $canHide = $('.can-hide')
 
-TweenMax.set($searchContainer,{right:"-800px", autoAlpha:0})
+TweenMax.set($searchContainer,{right:"-800px", autoAlpha:0, display: "none"})
 
 var showSearchContainer = function(){
-    TweenMax.to($searchContainer, .5, {autoAlpha: 1, right: 0, ease: Power2.easeOut})
+    TweenMax.to($searchContainer, .5, {autoAlpha: 1, right: 0, display: "block",ease: Power2.easeOut})
     TweenMax.to($searchLoupe, .5, {autoAlpha: 0, scaleX: 0, scaleY: 0})
     TweenMax.to($canHide, .5, {autoAlpha: 0, scaleX: 0, scaleY: 0})
 }
 
 var hideSearchContainer = function(){
-    TweenMax.to($searchContainer, .5, {autoAlpha: 0, right: "-800px", ease: Power2.easeOut})
+    TweenMax.to($searchContainer, .5, {autoAlpha: 0, right: "-800px", display: "none",ease: Power2.easeOut})
     TweenMax.to($searchLoupe, .5, {autoAlpha: 1, scaleX: 1, scaleY: 1})
     TweenMax.to($canHide, .5, {autoAlpha: 1, scaleX: 1, scaleY: 1})
 }
