daf7d9f0d393a80035552725cb801d6afcb0ecde
phil <phil@branch14.org>
2016-08-31 12:16:25 +0200

    initial version of remote control via rest instead of faye

--- app/controllers/api/devices_controller.rb
+++ app/controllers/api/devices_controller.rb
@@ -50,6 +50,11 @@ class Api::DevicesController < ApplicationController
                     identifier: @device.identifier,
                     interval: @device.heartbeat_interval
 
+    # payload might carry an event
+    if event = params[:event]
+      @device.send("can_#{event}?") and @device.send("#{event}!")
+    end
+
     render json: @device.details
   end
 
