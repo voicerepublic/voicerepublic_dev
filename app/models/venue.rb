d88dde13a3fa009fda30be08d568aebf02b28ce9
phil <phil@branch14.org>
2016-10-06 09:41:43 +0200

    an empty array is better than nil in that case

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -258,7 +258,7 @@ class Venue < ActiveRecord::Base
           title: talk.series.title,
           url: talk.series.self_url
         },
-        speakers: talks.speakers && talk.speakers.split(",").map(&:strip)
+        speakers: talks.speakers ? talk.speakers.split(",").map(&:strip) : []
       }
     end
   end
