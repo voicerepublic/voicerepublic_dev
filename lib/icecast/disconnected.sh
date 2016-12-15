fcd20d4e95637c8139fc23d14305f2006e3b6181
phil <phil@branch14.org>
2016-12-15 12:25:46 +0100

    improve icecast - step1

--- lib/icecast/disconnected.sh
+++ lib/icecast/disconnected.sh
@@ -2,7 +2,20 @@
 
 echo `date` disconnected >> /share/actions.log
 
-curl -X POST $CALLBACK_URL/disconnect/$CLIENT_TOKEN
+command="curl -X POST $CALLBACK_URL/disconnect/$CLIENT_TOKEN"
+
+n=0
+until [ $n -ge 10 ]
+do
+    $command && break
+    echo `date` "failed to report disconnected, retry" >> /share/actions.log
+    n=$[$n+1]
+done
+
+if [ n = 10 ]
+then
+    echo `date` "failed to report disconnected, giving up" >> /share/actions.log
+fi
 
 # TODO does it work for s3fs (fuse)
 # if not ditch s3fs and use s3/4cmd to upload here
@@ -10,4 +23,14 @@ sync
 
 echo `date` synced >> /share/actions.log
 
-curl -X POST $CALLBACK_URL/synced/$CLIENT_TOKEN
+command="curl -X POST $CALLBACK_URL/synced/$CLIENT_TOKEN"
+
+n=0
+until [ $n -ge 10 ]
+do
+    $command && exit
+    echo `date` "failed to report synced, retry" >> /share/actions.log
+    n=$[$n+1]
+done
+
+echo `date` "failed to report synced, giving up" >> /share/actions.log
