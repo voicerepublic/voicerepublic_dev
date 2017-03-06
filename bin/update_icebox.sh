2338ef8c2c70a630db5a7772ebc0d3df31d42e92
phil <phil@branch14.org>
2017-03-06 10:33:27 +0100

    fix permission problem

--- bin/update_icebox.sh
+++ bin/update_icebox.sh
@@ -10,7 +10,7 @@ echo      Hostname 35.157.47.150
 echo
 echo Replace the ip with your current instance on EC2.
 echo
-echo Any key to continue.
+echo Enter to continue.
 read
 
 echo "Activating root access..."
@@ -21,9 +21,13 @@ echo "The files have been copied to the icebox."
 echo "Preparing Icebox for imaging... (This might take a while.)"
 ssh root@icebox ./icecast/prepare_image.sh
 
-echo "Finished."
-echo "Now pull an image on EC2."
+echo "Finished. Next steps..."
 echo
-echo "Then add the AMI key to `config/settings.yml`."
-echo "Commit, Push, Deploy & Test it!"
+echo "1. Pull an image on EC2."
+echo "2. Add the AMI key to config/settings.yml."
+echo "3. Commit"
+echo "4. Push"
+echo "5. Deploy your branch."
+echo "6. Wait for the AMI to be 'available'."
+echo "7. Test it!"
 echo
