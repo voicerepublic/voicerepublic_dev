78753b1e928d4e194855dfdba261a494f428c666
phil <phil@branch14.org>
2017-03-27 10:21:20 +0200

    add semi-hidden link to venues' m3u8 playlist

--- lib/irbrc.rb
+++ lib/irbrc.rb
@@ -1,3 +1,10 @@
+def restart_streaming_server!(v)
+  v = Venue.find(v) if !v.is_a?(Venue)
+  v.reset!
+  v.reload.become_available!
+  v.reload.start_provisioning!
+end
+
 def list_venues
   Venue.not_offline.order('slug').each do |v|
     puts "%-10s %-45s %s" % [v.state, v.slug, v.stream_url]
@@ -5,9 +12,12 @@ def list_venues
   nil
 end
 
-def restart_streaming_server!(v)
-  v = Venue.find(v) if !v.is_a?(Venue)
-  v.reset!
-  v.reload.become_available!
-  v.reload.start_provisioning!
-end
+list_venues
+
+puts <<EOF
+Methods at your finger tips...
+------------------------------------
+* list_venues
+* restart_streaming_server! <venue>
+
+EOF
