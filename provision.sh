f1d6304f1ebe662650e10367e2a40967ba8730b6
phil <phil@branch14.org>
2017-03-28 10:06:47 +0200

    start x automatically

--- provision.sh
+++ provision.sh
@@ -7,9 +7,15 @@ apt-get update
 
 apt-get install -y git
 
+# auto login
 mkdir -p /etc/systemd/system/getty@tty1.service.d/
 cat > /etc/systemd/system/getty@tty1.service.d/override.conf <<EOF
 [Service]
 ExecStart=
 ExecStart=-/usr/bin/agetty --autologin vagrant --noclear %I \$TERM
 EOF
+
+apt-get install -y zsh i3
+
+# auto start x
+echo '[ "$(tty)" = "/dev/tty1" ] && exec startx' >> ~vagrant/.zprofile
