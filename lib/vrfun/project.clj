eac40bd2315c5dd71ec64c2e4973c36da3fa86e5
phil <phil@branch14.org>
2017-06-22 14:53:02 +0200

    Draft version of new upload by using s3-beam.
    Create duct project in order to use with s3-beam as
    signing service. Add cors middleware to enable signing
    services on different hosts.
    
    Fiddle around with s3-beam signing in order to make
    it work with v4 signing.

---
+++ lib/vrfun/project.clj
@@ -0,0 +1,26 @@
+(defproject vrfun "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [duct/core "0.6.1"]
+                 [duct/module.logging "0.3.0"]
+                 [duct/module.web "0.6.0"]
+                 [duct/module.ataraxy "0.2.0"]
+                 [org.martinklepsch/s3-beam "0.6.0-alpha3"]
+                 [io.netty/netty-all "4.0.27.Final"]]
+  :plugins [[duct/lein-duct "0.10.0"]]
+  :main ^:skip-aot vrfun.main
+  :resource-paths ["resources" "target/resources"]
+  :prep-tasks     ["javac" "compile" ["run" ":duct/compiler"]]
+  :profiles
+  {:dev  [:project/dev :profiles/dev]
+   :repl {:prep-tasks   ^:replace ["javac" "compile"]
+          :repl-options {:init-ns user}}
+   :uberjar {:aot :all}
+   :profiles/dev {}
+   :project/dev  {:source-paths   ["dev/src"]
+                  :resource-paths ["dev/resources"]
+                  :dependencies   [[integrant/repl "0.2.0"]
+                                   [eftest "0.3.1"]
+                                   [kerodon "0.8.0"]]}})
---
+++ lib/vrfun/src/vrfun/main.clj
@@ -0,0 +1,12 @@
+(ns vrfun.main
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [duct.core :as duct]))
+
+(duct/load-hierarchy)
+
+(defn -main [& args]
+  (let [keys (or (duct/parse-keys args) [:duct/daemon])]
+    (-> (duct/read-config (io/resource "vrfun/config.edn"))
+        (duct/prep keys)
+        (duct/exec keys))))
