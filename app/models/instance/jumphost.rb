4faa73c4aaeade3b1cd974d9c3c36f4f2ce64664
phil <phil@branch14.org>
2017-06-29 10:00:10 +0200

    more cleanup

--- app/models/instance/jumphost.rb
+++ app/models/instance/jumphost.rb
@@ -2,12 +2,4 @@ class Instance::Jumphost < Instance
 
   belongs_to :context, polymorphic: true # context is a device
 
-  def default_client_token
-    [ 'jh', device.identifier[0, 64-16], Time.now.to_i, generate_password(4) ] * '-'
-  end
-
-  def default_name
-    [ 'jh', device.name, generate_password(4) ] * '-'
-  end
-
 end
