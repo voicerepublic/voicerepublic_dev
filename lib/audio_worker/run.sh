1542b825f00473f2365cc22387d24caf7083d149
phil <phil@branch14.org>
2017-07-25 17:12:37 +0200

    more fixes

--- lib/audio_worker/run.sh
+++ lib/audio_worker/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-curl -o runner.rb $RUNNER_ENDPOINT
-
 aws configure set aws_access_key_id $AWS_ACCESS_KEY
 aws configure set aws_secret_access_key $AWS_SECRET_KEY
+
+echo Fetch updated runner from $RUNNER_ENDPOINT
+
+curl -o runner.rb $RUNNER_ENDPOINT
 
 ruby runner.rb
 
