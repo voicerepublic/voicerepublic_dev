754c10503f439b6316c025659431e4f76bcfa28e
Josef Erben <josef@erben.io>
2017-07-31 15:58:19 +0200

    Shutdown secret provider after build

--- build-images.sh
+++ build-images.sh
@@ -7,5 +7,6 @@ VERSION=0.0.1
 SUBJECT=some-unique-id
 USAGE="Usage: command -ihv args"
 
-docker run -p 172.17.0.1:14242:3000 -v ~/.ssh:/vault/.ssh dockito/vault &
+docker run --name vault -p 172.17.0.1:14242:3000 -v ~/.ssh:/vault/.ssh dockito/vault &
 docker build -t vr/dev .
+docker stop vault 
