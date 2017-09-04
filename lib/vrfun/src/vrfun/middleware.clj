9731f733f5a87336fe45e54f9041bcec0fd28a6c
Josef Erben <josef@erben.io>
2017-09-04 10:05:13 +0200

    Hook in cors middleware to allows signing from different urls

---
+++ lib/vrfun/src/vrfun/middleware.clj
@@ -0,0 +1,12 @@
+(ns vrfun.middleware
+
+(:require [integrant.core :as ig]
+            [ring.util.response :refer [header]]))
+
+(defmethod ig/init-key ::cors [_ _]
+  (fn [handler]
+    (fn [request]
+      (-> (handler request)
+          (header "Access-Control-Allow-Origin" "*")
+          (header "Access-Control-Allow-Methods" "GET")
+          (header "Access-Control-Allow-Headers" "Content-Type")))))
