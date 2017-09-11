ecaa130acf7c7e77b760cd10c8f1656a79dacafe
Josef Erben <josef@erben.io>
2017-09-11 17:38:11 +0200

    Run canonical request creation for multiple test cases

--- lib/vrfun/test/vrfun/aws4_auth_test.clj
+++ lib/vrfun/test/vrfun/aws4_auth_test.clj
@@ -27,6 +27,13 @@
       (s/split #"=")
       (last)))
 
+(def test-suite-dir  "vrfun/suite")
+
+(defn resource-string [name ext]
+  (-> (str test-suite-dir "/" name "/" name "." ext)
+      (clojure.java.io/resource)
+      (slurp)))
+
 ;; test data, global in whole test suite
 (def timestamp "20150830T123600Z")
 (def short-timestamp "20150830")
@@ -35,17 +42,44 @@
 (def secret-key "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY")
 (def access-key-id "AKIDEXAMPLE")
 
+(def test-cases '(
+                  "get-header-key-duplicate"
+                  ;"get-header-value-multiline"
+                  "get-header-value-order"
+                  "get-header-value-trim"
+                  "get-unreserved"
+                  "get-utf8"
+                  "get-vanilla"
+                  "get-vanilla-empty-query-key"
+                  "get-vanilla-query"
+                  "get-vanilla-query-order-key"
+                  "get-vanilla-query-order-key-case"
+                  "get-vanilla-query-order-value"
+                  "get-vanilla-query-unreserved"
+                  "get-vanilla-utf8-query"
+                  "post-header-key-case"
+                  "post-header-key-sort"
+                  "post-header-value-case"
+                  "post-vanilla"
+                  "post-vanilla-empty-query-value"
+                  "post-vanilla-query"
+                  ;"post-x-www-form-urlencoded"
+                  ;"post-x-www-form-urlencoded-parameters"
+                  ))
+
 (deftest post-vanilla
-  (let [{:keys [method uri headers]}
-        (parse-request (slurp
-                        (clojure.java.io/resource "vrfun/suite/post-vanilla/post-vanilla.req")))
+  (let [{:keys [method uri headers]} (parse-request (resource-string "post-vanilla" "req"))
         canonical-headers (sut/aws4-auth-canonical-headers headers)]
 
     (testing "creating canoncial request"
-      (is
-       (= (slurp (clojure.java.io/resource "vrfun/suite/post-vanilla/post-vanilla.creq"))
-          (sut/aws4-auth-canonical-request method uri (sut/aws4-auth-canonical-headers headers)))
-       "Returns valid canonical request"))
+      (doseq [name test-cases]
+        (prn name)
+        (let [input-string (resource-string name "req")
+              {:keys [method uri headers]} (parse-request input-string)]
+          (is
+           (= input-string)
+           (sut/aws4-auth-canonical-request method uri (sut/aws4-auth-canonical-headers headers)))
+          (str "Returns valid canonical request for " name))))
 
     (testing "creating string-to-sign"
       (is
@@ -55,11 +89,11 @@
 
     (testing "creating signature"
       (let [string-to-sign (sut/string-to-sign timestamp method uri
-                                             short-timestamp region service canonical-headers)]
-      (is
-       (= (parse-signature
-           (slurp (clojure.java.io/resource "vrfun/suite/post-vanilla/post-vanilla.authz")))
-          (sut/signature secret-key short-timestamp region service string-to-sign)))))
+                                               short-timestamp region service canonical-headers)]
+        (is
+         (= (parse-signature
+             (slurp (clojure.java.io/resource "vrfun/suite/post-vanilla/post-vanilla.authz")))
+            (sut/signature secret-key short-timestamp region service string-to-sign)))))
 
     (testing "creating authorization header")
     (is
