30f072cba6283f4b21aafff893ef43b3e5c1ad8a
Josef Erben <josef@erben.io>
2017-09-11 16:53:59 +0200

    Generify tests

--- lib/vrfun/test/vrfun/aws4_auth_test.clj
+++ lib/vrfun/test/vrfun/aws4_auth_test.clj
@@ -1,44 +1,67 @@
 (ns vrfun.aws4-auth-test
   (:require [vrfun.aws4-auth :as sut]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all]
+            [clojure.string :as s]))
 
 (deftest smoke-test
   (is (+ 2 2) 4))
 
+(defn to-header
+  "returns header map entry from string"
+  [acc next]
+  (let [[key val] (s/split next #":")]
+    (assoc acc key val)))
+
+(defn parse-request
+  "returns parsed request from .req file strings"
+  [req]
+  (let [line-wise (s/split req #"\n")
+        [method uri] (s/split (first line-wise) #" ")]
+    (merge {:method method
+            :uri uri}
+           (assoc {} :headers (reduce to-header {} (rest line-wise))))))
+
+(defn parse-signature
+  [authz]
+  (-> (re-find #"Signature=\S+" authz)
+      (s/split #"=")
+      (last)))
+
+;; test data, global in whole test suite
+(def timestamp "20150830T123600Z")
+(def short-timestamp "20150830")
+(def region "us-east-1")
+(def service "service")
+(def secret-key "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY")
+(def access-key-id "AKIDEXAMPLE")
+
 (deftest post-vanilla
-  (let [method "POST"
-        uri "/"
-        timestamp "20150830T123600Z"
-        short-timestamp "20150830"
-        region "us-east-1"
-        service "service"
-        secret-key "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY"
-        access-key-id "AKIDEXAMPLE"
-        headers {"HOST" "example.amazonaws.com"
-                 "X-Amz-Date" "20150830T123600Z"}
+  (let [{:keys [method uri headers]}
+        (parse-request (slurp
+                        (clojure.java.io/resource "vrfun/suite/post-vanilla/post-vanilla.req")))
         canonical-headers (sut/aws4-auth-canonical-headers headers)]
 
-    (testing "post-vanilla: canoncial request"
-
+    (testing "creating canoncial request"
       (is
        (= (slurp (clojure.java.io/resource "vrfun/suite/post-vanilla/post-vanilla.creq"))
           (sut/aws4-auth-canonical-request method uri (sut/aws4-auth-canonical-headers headers)))
        "Returns valid canonical request"))
 
-    (testing "post-vanilla: string-to-sign"
+    (testing "creating string-to-sign"
       (is
        (= (slurp (clojure.java.io/resource "vrfun/suite/post-vanilla/post-vanilla.sts"))
           (sut/string-to-sign timestamp method uri short-timestamp
                               region service canonical-headers))))
 
-    (testing "post-vanilla: signature"
+    (testing "creating signature"
       (let [string-to-sign (sut/string-to-sign timestamp method uri
                                              short-timestamp region service canonical-headers)]
       (is
-       (= "5da7c1a2acd57cee7505fc6676e4e544621c30862966e37dddb68e92efbe5d6b"
+       (= (parse-signature
+           (slurp (clojure.java.io/resource "vrfun/suite/post-vanilla/post-vanilla.authz")))
           (sut/signature secret-key short-timestamp region service string-to-sign)))))
 
-    (testing "post-vanilla: authorization header")
+    (testing "creating authorization header")
     (is
      (= (slurp (clojure.java.io/resource "vrfun/suite/post-vanilla/post-vanilla.authz"))
         (sut/aws4-authorisation method uri headers region service access-key-id secret-key)))))
