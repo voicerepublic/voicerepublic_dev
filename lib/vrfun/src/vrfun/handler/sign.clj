1535fc2468da9ece08f5c2678556286a5aed07b7
Josef Erben <josef@erben.io>
2017-09-19 15:01:33 +0200

    Get changes from aws4 library that make all unit tests pass.
    Also added some docstrings and reformated the files with cider.

--- lib/vrfun/src/vrfun/handler/sign.clj
+++ lib/vrfun/src/vrfun/handler/sign.clj
@@ -2,7 +2,7 @@
   (:require [ataraxy.core :as ataraxy]
             [ataraxy.response :as response]
             [integrant.core :as ig]
-            [vrfun.aws4-auth :refer [aws4-authorisation zone->host]])
+            [vrfun.aws4.auth :refer [authorize zone->host]])
   (:import [java.text DateFormat SimpleDateFormat]
            [java.util UUID Date TimeZone]))
 
@@ -14,10 +14,13 @@
     (.setTimeZone (TimeZone/getTimeZone "UTC"))))
 
 (defn- generate-random-uri
+  "Returns upload-path with random uuid"
   [file-name bucket]
   (clojure.string/join "/" (list "" bucket (UUID/randomUUID) file-name)))
 
-(defn- auth-header [file-name mime-type bucket aws-zone access-key secret-key]
+(defn- to-response
+  "Returns authorization header, upload-url and a map of the signed headers"
+  [file-name mime-type bucket aws-zone access-key secret-key]
   (let [headers {"Host" (zone->host aws-zone)
                  "x-amz-content-sha256" "UNSIGNED-PAYLOAD"
                  "x-amz-date" (.format iso8601-date-format (Date.))}
@@ -25,18 +28,23 @@
 
     {:headers
      (merge {:Authorization
-             (aws4-authorisation method uri "" headers ""
-                                 aws-zone service access-key secret-key)}
+             (authorize {:method method :uri uri :headers headers 
+                         :region aws-zone :service service
+                         :access-key access-key :secret secret-key})}
             headers)
      :upload-url (str "https://" (zone->host aws-zone) uri)
      :uri uri}))
 
-(defn- s3-sign-aws4 [bucket aws-zone access-key secret-key]
+(defn- s3-sign-aws4 
+  "Returns ring handler given signing configuration"
+  [bucket aws-zone access-key secret-key]
   (fn [{{:keys [mime-type file-name]} :params}]
     {:status 200
-     :body (pr-str (auth-header file-name mime-type bucket aws-zone access-key secret-key))}))
+     :body (pr-str (to-response file-name mime-type bucket aws-zone access-key secret-key))}))
 
-(defn- s3-sign []
+(defn- s3-sign 
+  "Reads configuration from environment variables and returns handler"
+  []
   (let [bucket (or (System/getenv "BUCKET") "vr-euc1-dev-audio-uploads")
         aws-zone (or (System/getenv "AWS_ZONE") "eu-central-1")
         access-key (or (System/getenv "ACCESS_KEY") "AKIAI7HSKHN3FBT2KAZQ")
