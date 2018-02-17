9823a18b8c3d29c8ca35b5322f3163106f4c3117
Alain M. Lafon <alain@200ok.ch>
2018-02-17 12:51:22 +0100

    Make rubocop happy

--- lib/podcaster.rb
+++ lib/podcaster.rb
@@ -1,7 +1,7 @@
 require 'tilt/builder'
 
+# Renders and persists RSS Podcast feeds
 class Podcaster
-
   def initialize
     @template = Tilt.new('app/views/shared/_podcast.rss.builder')
   end
@@ -11,47 +11,40 @@ class Podcaster
 
     puts "Title: #{talk.title}"
 
-    metadata = OpenStruct.new({talks: [talk],
+    metadata = OpenStruct.new(talks: [talk],
 
-                               # TODO
-                               category:    'Society & Culture',
+                              # TODO
+                              category:    'Society & Culture',
 
-                               # translations
-                               title:       I18n.t('talks.podcast.title', title: talk.title),
-                               image_title: I18n.t('talks.podcast.title', title: talk.title),
+                              # translations
+                              title:       I18n.t('talks.podcast.title', title: talk.title),
+                              image_title: I18n.t('talks.podcast.title', title: talk.title),
 
-                               # misc
-                               description: talk.description_as_text,
-                               author:      talk.user.name,
-                               subtitle:    talk.teaser,
+                              # misc
+                              description: talk.description_as_text,
+                              author:      talk.user.name,
+                              subtitle:    talk.teaser,
 
-                               # urls
-                               url:         Rails.application.routes.url_helpers.talk_url(talk),
-                               image_link:  Rails.application.routes.url_helpers.talk_url(talk),
-                               rss_url:     Rails.application.routes.url_helpers.talk_url(talk, format: :rss),
-                               image:       talk.image})
+                              # urls
+                              url:         Rails.application.routes.url_helpers.talk_url(talk),
+                              image_link:  Rails.application.routes.url_helpers.talk_url(talk),
+                              rss_url:     Rails.application.routes.url_helpers.talk_url(talk, format: :rss),
+                              image:       talk.image)
 
-    puts "before render template"
     podcast_str = @template.render metadata
-    puts "after render template"
-    File.open(Rails.root.join('public/feeds/talks', "#{id}.rss"), 'wb') do |file|
-      file << podcast_str
+    File.open(Rails.root.join('public/feeds/talks', "#{id}.rss"), 'wb') do |f|
+      f << podcast_str
     end
-    puts "all good!"
-
   end
 
   def self.itunes_image_url(image)
     image.thumb('1400x1400#', format: 'png').url(name: 'image.png')
   end
 
-
   def self.unsecured_url(url)
-   url.gsub(/https:\/\//, 'http://')
+    url.gsub(%r{https:\/\/}, 'http://')
   end
-
 end
-
 
 # podcaster = Podcaster.new
 # podcaster.render_for_talk(2)
