b9f6c43506a67dd0eb845e6b709aad043a4b36fa
Norbert Preining <norbert@preining.info>
2020-07-06 11:48:28 +0900

    add javax.xml.bind for lein/clojure on java>8

--- lib/vrng/project.clj
+++ lib/vrng/project.clj
@@ -14,6 +14,7 @@
                  [secretary "1.2.3"]
                  [figwheel-sidecar "0.5.4-7"]
                  [org.clojure/core.incubator "0.1.3"]
+                 [javax.xml.bind/jaxb-api "2.4.0-b180830.0359"]
                  [cljs-ajax "0.5.4"]
                  [cljsjs/moment "2.10.6-4"]
                  [cljsjs/selectize "0.12.1-1"]
