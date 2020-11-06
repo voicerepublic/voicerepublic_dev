b5be96e545223e6756c45973a181b6aaf97cdea8
Arvind kushwah <arvindkushwah9@gmail.com>
2020-11-06 19:24:01 +0530

    Project is complete runging but design not correct

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = true
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -104,7 +104,7 @@ Rails.application.configure do
     end
   end
 
-  config.assets.js_compressor = BypassableUglifier
+  # config.assets.js_compressor = BypassableUglifier
 
 
   # Optionally disable Javascript/CSS compression
